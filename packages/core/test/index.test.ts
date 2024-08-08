d62d0b566a4be937e69c3d8f0d41d7dd19de4315
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-08-08 11:19:24 +0100

    Test system actions and events

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,15 +1,61 @@
 import { createFramework } from '../src/index';
 //TODO: Figure out jest ts support with ts-jest
 import { describe, expect, it } from '@jest/globals';
+import { IntegrationAction, IntegrationEvent } from '../src/types';
+import { z } from 'zod';
+
+const testFrameworkName = 'TEST_FRAMEWORK';
+const testPluginName = 'TEST_PLUGIN';
+const testActionType = 'TEST_ACTION';
+const testEventKey = 'TEST_EVENT';
+
+const mockSystemActions: IntegrationAction[] = [
+  {
+    pluginName: testPluginName,
+    schema: z.object({}),
+    outputSchema: z.object({}),
+    type: testActionType,
+    label: 'test',
+    description: 'test',
+    category: 'test',
+    executor: async () => {},
+    isHidden: false,
+  },
+];
+
+const mockSystemEvents: IntegrationEvent[] = [
+  {
+    key: testEventKey,
+    schema: z.object({}),
+    triggerProperties: {
+      type: testEventKey,
+      label: 'test',
+      description: 'test',
+    },
+  },
+];
+
+const integrationFramework = createFramework({
+  name: testFrameworkName,
+  plugins: [],
+  SystemActions: mockSystemActions,
+  SystemEvents: mockSystemEvents,
+});
 
 describe('createFramework', () => {
-  it('should return a framework object', () => {
-    const framework = createFramework({
-      name: 'test',
-      plugins: [],
-      SystemActions: [],
-      SystemEvents: [],
-    });
-    expect(framework).toBeDefined();
+  it('should create a framework', () => {
+    expect(integrationFramework).toBeDefined();
+  });
+
+  it('shold register system actions', () => {
+    expect(
+      Object.values(integrationFramework.getSystemActions() ?? {})
+    ).toEqual(mockSystemActions);
+  });
+
+  it('shold register system events', () => {
+    expect(Object.values(integrationFramework.getSystemEvents() ?? {})).toEqual(
+      mockSystemEvents
+    );
   });
 });
