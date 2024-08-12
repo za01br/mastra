05f4f249da06f6b7634e8ab2a4e1910db17c38df
Shane Thomas <shane@codekarate.com>
2024-08-12 16:10:44 -0500

    Update config to add empty db property

--- packages/admin/example.future.config.ts
+++ packages/admin/example.future.config.ts
@@ -5,9 +5,12 @@ import { IntegrationPlugin } from '../core/src/plugin';
 
 export const config: Config = {
   name: 'kepler',
-  //dbConnectionConfig: {}, // TODO: Add this
   //logConfig: {}, // TODO: Add this
   systemActions: [] as IntegrationAction[],
   systemEvents: [] as IntegrationEvent[],
-  plugins: [] as IntegrationPlugin[], // TODO will update after plugins are moved.
+  plugins: [] as IntegrationPlugin[],
+  db: {
+    provider: '',
+    uri: '',
+  },
 };
