1d4ec24a2a68953df1ed50819a802a3b2b369026
Kenny <adelekekehinde06@gmail.com>
2024-08-12 12:01:58 +0100

    feat: update sidebar

--- packages/admin/src/app/components/sidebar.tsx
+++ packages/admin/src/app/components/sidebar.tsx
@@ -1,3 +1,5 @@
 export const Sidebar = () => {
-    return <></>
+    return <div className="relative z-20 h-full text-light-text">
+        <div className="bg-main-bg flex h-full w-full p-4"></div>
+    </div>
 }
