c6188fb14d8f4c416bb156e128981ae386308f04
Kenny <adelekekehinde06@gmail.com>
2024-08-12 15:05:41 +0100

    feat: update layout

--- packages/admin/src/app/layout.tsx
+++ packages/admin/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Sidebar } from "./components/sidebar";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Sidebar } from './components/sidebar';
+import './globals.css';
 
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -12,16 +12,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className}`}>
         <main className="bg-main-bg grid h-full w-full grid-cols-[15rem_minmax(0,_1fr)] overflow-clip">
           <div className="z-20 h-full">
-            <div className="h-full"><Sidebar/></div>
+            <div className="h-full">
+              <Sidebar />
+            </div>
           </div>
-          <div className="bg-window-bg grid place-items-center border-primary-border rounded-xs border-thin m-2 overflow-hidden border-solid">
+          <div className="bg-window-bg grid p-4 border-primary-border rounded-xs border-thin m-2 overflow-hidden border-solid">
             {children}
-          </div> 
+          </div>
         </main>
-       </body>
+      </body>
     </html>
   );
 }
