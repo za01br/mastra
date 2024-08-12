a602b9fdb7d52001b2de78f5a75548bde788e5fa
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-08-12 13:55:26 -0700

    DI, DICred

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,21 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import { IntegrationPlugin } from './plugin';
 import {
-  IntegrationAction,
-  IntegrationActionExcutorParams,
-  IntegrationEvent,
+    IntegrationAction,
+    IntegrationActionExcutorParams,
+    IntegrationEvent,
 } from './types';
 import { omitBy } from 'lodash';
+import { DataLayer } from './data-access';
 
 export interface Config {
-  name: string;
-  db: {
-    provider: string,
-    uri: string
-  },
-  plugins: IntegrationPlugin[];
-  systemActions: IntegrationAction[];
-  systemEvents: IntegrationEvent[];
+    name: string;
+    db: {
+        provider: string,
+        uri: string
+    },
+    plugins: IntegrationPlugin[];
+    systemActions: IntegrationAction[];
+    systemEvents: IntegrationEvent[];
 }
 
 export const CORE_PLUGIN_NAME = 'SYSTEM';
@@ -23,178 +24,194 @@ export const CORE_PLUGIN_NAME = 'SYSTEM';
 export * from './types'
 
 class IntegrationFramework {
-  //global events grouped by plugin
-  globalEvents: Map<string, Record<string, IntegrationEvent>> = new Map();
-  // global event handlers
-  globalEventHandlers: any[] = [];
-  // global actions grouped by plugin
-  globalActions: Map<string, Record<string, IntegrationAction<any>>> =
-    new Map();
-  plugins: Map<string, IntegrationPlugin> = new Map();
+    //global events grouped by plugin
+    globalEvents: Map<string, Record<string, IntegrationEvent>> = new Map();
+    // global event handlers
+    globalEventHandlers: any[] = [];
+    // global actions grouped by plugin
+    globalActions: Map<string, Record<string, IntegrationAction<any>>> =
+        new Map();
+    plugins: Map<string, IntegrationPlugin> = new Map();
 
-  registerPlugin(pluginDefinition: IntegrationPlugin) {
-    const { name } = pluginDefinition;
-    this.plugins.set(name, pluginDefinition);
+    dataLayer: DataLayer
 
-    pluginDefinition.defineEvents();
-
-    this.registerEvents({
-      events: Object.values(pluginDefinition.getEvents()),
-      pluginName: name,
-    });
-
-    pluginDefinition.defineActions();
-
-    this.registerActions({
-      actions: Object.values(pluginDefinition.getActions()),
-      pluginName: name,
-    });
-
-    this.globalEventHandlers.push(...pluginDefinition.getEventHandlers());
-  }
-
-  registerEvents({
-    events,
-    pluginName = CORE_PLUGIN_NAME,
-  }: {
-    events: IntegrationEvent[];
-    pluginName?: string;
-  }) {
-    const pluginEvents = this.globalEvents.get(pluginName) || {};
-    this.globalEvents.set(pluginName, {
-      ...pluginEvents,
-      ...events.reduce((acc, event) => ({ ...acc, [event.key]: event }), {}),
-    });
-  }
-
-  registerActions({
-    actions,
-    pluginName = CORE_PLUGIN_NAME,
-  }: {
-    actions: IntegrationAction[];
-    pluginName?: string;
-  }) {
-    const pluginActions = this.globalActions.get(pluginName) || {};
-    this.globalActions.set(pluginName, {
-      ...pluginActions,
-      ...actions.reduce(
-        (acc, action) => ({ ...acc, [action.type]: action }),
-        {}
-      ),
-    });
-  }
-
-  availablePlugins() {
-    return Array.from(this.plugins.entries()).map(([name, plugin]) => {
-      return {
-        name,
-        plugin,
-      };
-    });
-  }
-
-  getPlugin(name: string) {
-    return this.plugins.get(name);
-  }
-
-  getGlobalEvents() {
-    return this.globalEvents;
-  }
-
-  getSystemEvents() {
-    const events = this.globalEvents.get(CORE_PLUGIN_NAME);
-    return omitBy(events, (value) => value.triggerProperties?.isHidden);
-  }
-
-  getEventsByPlugin(name: string) {
-    return this.globalEvents.get(name);
-  }
-
-  getGlobalEventHandlers() {
-    return this.globalEventHandlers;
-  }
-
-  getActions() {
-    return this.globalActions;
-  }
-
-  getSystemActions() {
-    return this.globalActions.get(CORE_PLUGIN_NAME);
-  }
-
-  getActionsByPlugin(name: string, includeHidden?: boolean) {
-    const pluginActions = this.globalActions.get(name);
-
-    if (includeHidden) {
-      return pluginActions;
-    }
-    return omitBy(pluginActions, (value) => value.isHidden);
-  }
-
-  async executeAction({
-    pluginName = CORE_PLUGIN_NAME,
-    action,
-    payload,
-  }: {
-    pluginName?: string;
-    action: string;
-    payload: IntegrationActionExcutorParams<any>;
-  }) {
-    if (pluginName === CORE_PLUGIN_NAME) {
-      const actionExecutor = this.globalActions.get(CORE_PLUGIN_NAME)?.[action];
-
-      if (!actionExecutor) {
-        throw new Error(`No global action exists for ${action}`);
-      }
-
-      return actionExecutor.executor(payload);
+    constructor({ dataLayer }: { dataLayer: DataLayer }) {
+        this.dataLayer = dataLayer
     }
 
-    const plugin = this.getPlugin(pluginName);
-    if (!plugin) {
-      throw new Error(`No plugin exists for ${pluginName}`);
+    async connectedPlugins({ context }: { context: { connectionId: string } }) {
+        const plugins = this.availablePlugins();
+        const connectionChecks = await Promise.all(
+            plugins.map(async ({ plugin }) => {
+                const connection = await this.dataLayer.getConnectionById({ connectionId: context.connectionId, name: plugin.name });
+                return { plugin, connected: !!connection };
+            }),
+        );
+        return connectionChecks.filter(({ connected }) => connected).map(({ plugin }) => plugin);
     }
 
-    const actionExecutor = plugin.getActions()?.[action];
+    registerPlugin(pluginDefinition: IntegrationPlugin) {
+        const { name } = pluginDefinition;
+        this.plugins.set(name, pluginDefinition);
 
-    if (!actionExecutor) {
-      throw new Error(`No action exists for ${action} in ${pluginName}`);
+        pluginDefinition.defineEvents();
+
+        this.registerEvents({
+            events: Object.values(pluginDefinition.getEvents()),
+            pluginName: name,
+        });
+
+        pluginDefinition.defineActions();
+
+        this.registerActions({
+            actions: Object.values(pluginDefinition.getActions()),
+            pluginName: name,
+        });
+
+        this.globalEventHandlers.push(...pluginDefinition.getEventHandlers());
     }
 
-    return actionExecutor.executor(payload);
-  }
+    registerEvents({
+        events,
+        pluginName = CORE_PLUGIN_NAME,
+    }: {
+        events: IntegrationEvent[];
+        pluginName?: string;
+    }) {
+        const pluginEvents = this.globalEvents.get(pluginName) || {};
+        this.globalEvents.set(pluginName, {
+            ...pluginEvents,
+            ...events.reduce((acc, event) => ({ ...acc, [event.key]: event }), {}),
+        });
+    }
+
+    registerActions({
+        actions,
+        pluginName = CORE_PLUGIN_NAME,
+    }: {
+        actions: IntegrationAction[];
+        pluginName?: string;
+    }) {
+        const pluginActions = this.globalActions.get(pluginName) || {};
+        this.globalActions.set(pluginName, {
+            ...pluginActions,
+            ...actions.reduce(
+                (acc, action) => ({ ...acc, [action.type]: action }),
+                {}
+            ),
+        });
+    }
+
+    availablePlugins() {
+        return Array.from(this.plugins.entries()).map(([name, plugin]) => {
+            return {
+                name,
+                plugin,
+            };
+        });
+    }
+
+    getPlugin(name: string) {
+        return this.plugins.get(name);
+    }
+
+    getGlobalEvents() {
+        return this.globalEvents;
+    }
+
+    getSystemEvents() {
+        const events = this.globalEvents.get(CORE_PLUGIN_NAME);
+        return omitBy(events, (value) => value.triggerProperties?.isHidden);
+    }
+
+    getEventsByPlugin(name: string) {
+        return this.globalEvents.get(name);
+    }
+
+    getGlobalEventHandlers() {
+        return this.globalEventHandlers;
+    }
+
+    getActions() {
+        return this.globalActions;
+    }
+
+    getSystemActions() {
+        return this.globalActions.get(CORE_PLUGIN_NAME);
+    }
+
+    getActionsByPlugin(name: string, includeHidden?: boolean) {
+        const pluginActions = this.globalActions.get(name);
+
+        if (includeHidden) {
+            return pluginActions;
+        }
+        return omitBy(pluginActions, (value) => value.isHidden);
+    }
+
+    async executeAction({
+        pluginName = CORE_PLUGIN_NAME,
+        action,
+        payload,
+    }: {
+        pluginName?: string;
+        action: string;
+        payload: IntegrationActionExcutorParams<any>;
+    }) {
+        if (pluginName === CORE_PLUGIN_NAME) {
+            const actionExecutor = this.globalActions.get(CORE_PLUGIN_NAME)?.[action];
+
+            if (!actionExecutor) {
+                throw new Error(`No global action exists for ${action}`);
+            }
+
+            return actionExecutor.executor(payload);
+        }
+
+        const plugin = this.getPlugin(pluginName);
+        if (!plugin) {
+            throw new Error(`No plugin exists for ${pluginName}`);
+        }
+
+        const actionExecutor = plugin.getActions()?.[action];
+
+        if (!actionExecutor) {
+            throw new Error(`No action exists for ${action} in ${pluginName}`);
+        }
+
+        return actionExecutor.executor(payload);
+    }
 }
 
 export function createFramework(config: Config) {
-  console.log(JSON.stringify(config, null, 2));
-  let db
+    console.log(JSON.stringify(config, null, 2));
+    let db
 
-  if (config.db.provider === 'postgres') {
-    db = new PrismaClient({ datasources: { db: { url: config.db.uri }} })
-  }
-  
-  if (!db) {
-    throw new Error('No database config/provider found')
-  }
+    if (config.db.provider === 'postgres') {
+        db = new PrismaClient({ datasources: { db: { url: config.db.uri } } })
+    }
 
-  console.log(db.field)
-  
-  const framework = new IntegrationFramework();
+    if (!db) {
+        throw new Error('No database config/provider found')
+    }
 
-  // Register plugins
-  config.plugins.forEach((plugin) => {
-    framework.registerPlugin(plugin);
-  });
+    const dataLayer = new DataLayer({ db })
+    const framework = new IntegrationFramework({ dataLayer });
 
-  // Register System actions
-  framework.registerActions({
-    actions: config.systemActions,
-  });
+    // Register plugins
+    config.plugins.forEach((plugin) => {
+        framework.registerPlugin(plugin);
+    });
 
-  // Register System events
-  framework.registerEvents({
-    events: config.systemEvents,
-  });
+    // Register System actions
+    framework.registerActions({
+        actions: config.systemActions,
+    });
 
-  return framework;
+    // Register System events
+    framework.registerEvents({
+        events: config.systemEvents,
+    });
+
+    return framework;
 }
