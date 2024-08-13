b3bbe3528b13fb53664b17379daabc29d009be7f
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2024-08-13 14:53:19 +0100

    add padding

--- packages/admin/src/app/records/page.tsx
+++ packages/admin/src/app/records/page.tsx
@@ -3,5 +3,5 @@ import { getIntegrations } from './get-integration';
 export default async function Records() {
   const integrations = await getIntegrations();
 
-  return <h1 className="text-light-text">Records</h1>;
+  return <h1 className="text-light-text p-4">Records</h1>;
 }
