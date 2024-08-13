eba1231b0f0516becb473626ac34824be7847e42
Kenny <adelekekehinde06@gmail.com>
2024-08-13 13:07:54 +0100

    feat: add types for mock config

---
+++ packages/admin/src/app/records/types.ts
@@ -0,0 +1,24 @@
+export interface Config {
+  integrations: Integration[];
+}
+
+export interface Integration {
+  name: string;
+  id: string;
+  icon: string;
+  displayConfig: DisplayConfig;
+}
+
+export interface DisplayConfig {
+  gridView: GridView;
+}
+
+export interface GridView {
+  columns: Column[];
+}
+
+export interface Column {
+  field: string;
+  header: string;
+  type: string;
+}
