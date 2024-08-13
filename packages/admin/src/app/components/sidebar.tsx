3f98adeb100428bf0138d21d6ad0c01eea9eef8c
Kenny <adelekekehinde06@gmail.com>
2024-08-13 10:19:01 +0100

    feat: use sprites

--- packages/admin/src/app/components/sidebar.tsx
+++ packages/admin/src/app/components/sidebar.tsx
@@ -4,34 +4,45 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import { Icon } from '@/components/ui/svg/icon';
+
 import logo from '@/icons/logo.svg';
-import logs from '@/icons/logs.svg';
-import records from '@/icons/records.svg';
 import search from '@/icons/search.svg';
-import workflow from '@/icons/workflow.svg';
 
 import { cn } from '@/lib/utils';
 
-const links = [
-  { name: 'workflows', url: 'workflows', icon: workflow },
-  { name: 'logs', url: 'logs', icon: logs },
+import { IconName } from '@/types/icons';
+
+const links: Array<{
+  name: string;
+  url: string;
+  icon: IconName;
+}> = [
+  { name: 'workflows', url: 'workflows', icon: 'workflow' },
+  { name: 'logs', url: 'logs', icon: 'logs' },
   {
     name: 'records',
     url: 'records',
-    icon: records,
+    icon: 'records',
   },
 ];
 
-function Tab({ text, url, isActive, icon }: { text: string; url: string; isActive: boolean; icon: string }) {
+function Tab({ text, url, isActive, icon }: { text: string; url: string; isActive: boolean; icon: IconName }) {
   return (
     <Link
       href={url}
       className={cn(
         'flex w-full px-2 items-center gap-3 transition-all rounded-xs group text-small hover:bg-light-text/5',
-        isActive ? 'bg-light-text/5 ' : '',
+        isActive ? 'bg-light-text/5' : '',
       )}
     >
-      <Image src={icon} alt="strematic logo" width={21} height={21} className="w-[0.875rem] h-[0.875rem]" priority />
+      <Icon
+        name={icon}
+        className={cn(
+          'w-[0.875rem] h-[0.875rem] text-dim-text group-hover:text-light-text',
+          isActive ? 'text-light-text' : '',
+        )}
+      />
       <p
         className={cn(
           'py-[0.38rem] group-hover:text-light-text transition-all text-dim-text capitalize ',
