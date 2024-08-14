fbd69a0f29f95e6182e42a326db6b1beafa6eb0a
Andrew Bettke <abettke@gmail.com>
2024-08-14 15:49:17 -0400

    FUTR-65: Inject framework access to registered route handlers.

--- packages/admin/src/app/integrations/client-row.tsx
+++ packages/admin/src/app/integrations/client-row.tsx
@@ -1,38 +1,38 @@
-'use-client'
+'use client';
 
-import { Button } from "@/components/ui/button"
-import { buildRedirectUri, connectIntegration } from "./actions"
-import { useCallback, useState } from "react";
+import { useCallback, useState } from 'react';
 
-export function IntegrationRow({ integration }: { integration: any }) {
-    const name = integration.name
-    const [isConnecting, setIsConnecting] = useState(false);
-    const [isConnectingManually, setIsConnectingManually] = useState(false);
+import { Button } from '@/components/ui/button';
 
-    const handleConnect = useCallback(async () => {
-        setIsConnecting(true);
-        try {
-            window.location.assign(await buildRedirectUri({ name, connectionId: '1', clientRedirectPath: 'import-data' }));
-        } catch (err) {
-            console.error(err);
-        } finally {
-            setIsConnecting(false);
-        }
-    }, [isConnectingManually, name]);
+import { buildRedirectUri } from './actions';
 
+type IntegrationRowProps = {
+  name: string;
+};
 
-    return (
-        <div className="flex">
-            <div className="flex-1 self-center">
-                <p key={integration.name}>{integration.name}</p>
-            </div>
-            <Button
-                className="h-[31px] text-xs"
-                size="sm"
-                onClick={handleConnect}
-            >
-                Connect
-            </Button>
-        </div>
-    )
+export function IntegrationRow({ name }: IntegrationRowProps) {
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isConnectingManually, setIsConnectingManually] = useState(false);
+
+  const handleConnect = useCallback(async () => {
+    setIsConnecting(true);
+    try {
+      window.location.assign(await buildRedirectUri({ name, connectionId: '1', clientRedirectPath: 'import-data' }));
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsConnecting(false);
+    }
+  }, [isConnectingManually, name]);
+
+  return (
+    <div className="flex">
+      <div className="flex-1 self-center">
+        <p key={name}>{name}</p>
+      </div>
+      <Button className="h-[31px] text-xs" size="sm" onClick={handleConnect}>
+        Connect
+      </Button>
+    </div>
+  );
 }
