6980f7b6e649285bc0135b2cfa0c3e57ba1188c3
Andrew Bettke <abettke@gmail.com>
2024-08-14 11:14:44 -0400

    FUTR-65: Setup local inngest dev server to test example function registration.

--- packages/core/src/next/inngest.ts
+++ packages/core/src/next/inngest.ts
@@ -8,7 +8,16 @@ const client = new Inngest({
   id: APP_ID,
 });
 
+export const helloWorld = client.createFunction(
+  { id: 'hello-world' },
+  { event: 'test/hello.world' },
+  async ({ event, step }) => {
+    await step.sleep('wait-a-moment', '1s');
+    return { event, body: 'Hello, World!' };
+  }
+);
+
 export default serve({
   client,
-  functions: [],
+  functions: [helloWorld],
 });
