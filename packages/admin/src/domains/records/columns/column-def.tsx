38a7ad7933ab9fe92ca5962f986380df1ab51604
Kenny <adelekekehinde06@gmail.com>
2024-08-14 13:12:49 +0100

    feat: create column definition

---
+++ packages/admin/src/domains/records/columns/column-def.tsx
@@ -0,0 +1,29 @@
+import { CellContext } from '@tanstack/react-table';
+
+import { IconName } from '@/types/icons';
+
+import { CellComponent } from './cell-component';
+import { HeaderCell } from './header-cell';
+
+export const createColumnDef = ({ fields }: { fields: any[] }) => {
+  const columnDef = fields.slice().map((field, index) => {
+    const { name, displayName, type, id } = field;
+
+    return {
+      id,
+      header: <HeaderCell icon={fieldTypeToIconMap[type] as IconName} header={displayName} />,
+      cell: (cellContext: CellContext<any, unknown>) => {
+        <CellComponent field={field} cellContext={cellContext} index={index} />;
+      },
+      accessorKey: name,
+      field,
+      minSize: index === 0 ? 200 : 50,
+    };
+  });
+
+  return columnDef;
+};
+
+export const fieldTypeToIconMap: Record<string, string> = {
+  SINGLE_LINE_TEXT: 'text',
+};
