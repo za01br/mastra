0144d97c32a8ad68c928d026c99fc945b5a3d497
Sam Bhagwat <calcsam@gmail.com>
2024-08-14 14:38:36 -0700

    get dev command working

--- packages/cli/source/commands/dev.tsx
+++ packages/cli/source/commands/dev.tsx
@@ -1,6 +1,64 @@
 import React from 'react';
 import {Text} from 'ink';
 
-export default function Login() {
+import {execa, ExecaError} from 'execa';
+
+async function startNextDevServer() {
+	function log(message: any, isError = false) {
+		const timestamp = new Date().toISOString();
+		const formattedMessage = `[${timestamp}] ${message}\n`;
+
+		if (isError) {
+			process.stderr.write(formattedMessage);
+		} else {
+			process.stdout.write(formattedMessage);
+		}
+	}
+
+	log('Starting Next.js dev server...');
+
+	try {
+		// TODO: get output re-colorized
+		// TODO: better cwd?
+		// TODO: test in diff repo
+		const nextServer = execa('npm run dev --port=3456', {
+			cwd: './node_modules/@arkw/admin',
+			all: true,
+			buffer: false,
+			env: process.env,
+			shell: true,
+		});
+
+		nextServer.all.on('data', (data: any) => {
+			const output = data.toString().trim();
+			log(output);
+
+			if (output.includes('compiled successfully')) {
+				log('Next.js dev server is ready!');
+			}
+		});
+
+		process.on('SIGINT', async () => {
+			log('Stopping Next.js dev server...');
+			await nextServer.kill();
+			process.exit();
+		});
+
+		await nextServer;
+	} catch (error: any) {
+		if (error instanceof ExecaError) {
+			console.log(error);
+		}
+		log(`Error: ${error.message}`, true);
+		if (error.stderr) {
+			log(`stderr: ${error.stderr}`, true);
+		}
+	} finally {
+	}
+}
+
+startNextDevServer().catch(console.error);
+
+export default function Dev() {
 	return <Text>This should open up the Next.js dev server on port 3456</Text>;
 }
