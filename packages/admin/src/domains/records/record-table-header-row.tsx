f545a949748e415a20df5630b3ee39f6dc13ab6a
Kenny <adelekekehinde06@gmail.com>
2024-08-14 17:53:56 +0100

    feat: add record table header row
    the row rendered for every item

--- packages/admin/src/domains/records/record-table-header-row.tsx
+++ packages/admin/src/domains/records/record-table-header-row.tsx
@@ -7,21 +7,19 @@ import { useTableContext } from './context/table-context';
 import { HeaderCell } from './record-table-header-cell';
 
 export function HeaderRow<TData, TValue>({ headerRow }: { headerRow: HeaderGroup<TData> }) {
-  const { setRowSelection, table } = useTableContext();
+  const { table, setRowSelection } = useTableContext();
 
   if (!table) return <></>;
 
   return (
-    <TableRow key={headerRow.id} className="border-kp-border-1 relative flex border border-b-0 border-l-0 border-t-0">
-      {!!setRowSelection && (
-        <TableHead className="border-kp-border-1 bg-kp-bg-2 sticky left-0 z-40 flex h-11 w-[3rem] items-center justify-center border-b p-0">
-          <Checkbox
-            checked={table.getIsSomePageRowsSelected() && 'indeterminate'}
-            // onCheckedChange={toggleValidRows}
-            aria-label="Select all"
-          />
-        </TableHead>
-      )}
+    <TableRow key={headerRow.id} className="relative flex border-none">
+      <TableHead className="border-b-primary-border border-b-[0.5px] bg-kp-bg-2 sticky left-0 z-40 flex h-11 w-[3rem] items-center justify-center p-0">
+        <Checkbox
+          checked={table.getIsSomePageRowsSelected() && 'indeterminate'}
+          // onCheckedChange={toggleValidRows}
+          aria-label="Select all"
+        />
+      </TableHead>
       {headerRow.headers.map((headerCell, i: number) => (
         <HeaderCell index={i} headerCell={headerCell} key={i} />
       ))}
