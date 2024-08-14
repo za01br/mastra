6d5779296ebbe80d799f847f4c3e83aab8458e3b
Kenny <adelekekehinde06@gmail.com>
2024-08-14 17:53:00 +0100

    feat: create and use client layout

--- packages/admin/src/app/records/[integration]/page.tsx
+++ packages/admin/src/app/records/[integration]/page.tsx
@@ -1,25 +1,12 @@
 import { getConfig } from '@/lib/get-configuration';
 
-import { createColumnDef } from '@/domains/records/columns/column-def';
-import { RecordTable } from '@/domains/records/record-table';
+import { ClientLayout } from './client-layout';
 
 export default async function Integration({ params }: { params: { integration: string } }) {
   const config = await getConfig();
   const integrationConfig = config.integrations.find(config => config.name.toLowerCase() === params.integration);
   const fields = integrationConfig?.displayConfig.gridView.fields;
+  const data = integrationConfig?.displayConfig.gridView.data;
 
-  let cols = {};
-
-  if (fields) {
-    cols = createColumnDef({ fields });
-  }
-
-  return (
-    <section>
-      <h1 className="text-sm gradient h-fit capitalize border-b-[0.5px] py-2 border-primary-border p-4">
-        {params.integration}
-      </h1>
-      <RecordTable columns={fields!} data={[]} />
-    </section>
-  );
+  return <ClientLayout integration={params.integration} fields={fields!} data={data!} />;
 }
