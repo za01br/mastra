e12f64058766024048c42fa98ae806b3b45b89a4
Kenny <adelekekehinde06@gmail.com>
2024-08-14 13:08:33 +0100

    feat: update integration page

--- packages/admin/src/app/records/[integration]/page.tsx
+++ packages/admin/src/app/records/[integration]/page.tsx
@@ -1,4 +1,25 @@
+import { getConfig } from '@/lib/get-configuration';
+
+import { createColumnDef } from '@/domains/records/columns/column-def';
+import { RecordTable } from '@/domains/records/record-table';
+
 export default async function Integration({ params }: { params: { integration: string } }) {
-  // get field and data
-  return <h1 className="text-light-text capitalize p-4">{params.integration}</h1>;
+  const config = await getConfig();
+  const integrationConfig = config.integrations.find(config => config.name.toLowerCase() === params.integration);
+  const fields = integrationConfig?.displayConfig.gridView.fields;
+
+  let cols = {};
+
+  if (fields) {
+    cols = createColumnDef({ fields });
+  }
+
+  return (
+    <section>
+      <h1 className="text-sm gradient h-fit capitalize border-b-[0.5px] py-2 border-primary-border p-4">
+        {params.integration}
+      </h1>
+      <RecordTable columns={fields!} data={[]} />
+    </section>
+  );
 }
