2ca4e55b3403859fed4324cf42e588c8bf009609
Kenny <adelekekehinde06@gmail.com>
2024-08-14 17:55:28 +0100

    fix: component not being rendered

--- packages/admin/src/domains/records/columns/column-def.tsx
+++ packages/admin/src/domains/records/columns/column-def.tsx
@@ -5,6 +5,10 @@ import { IconName } from '@/types/icons';
 import { CellComponent } from './cell-component';
 import { HeaderCell } from './header-cell';
 
+export const fieldTypeToIconMap: Record<string, string> = {
+  SINGLE_LINE_TEXT: 'text',
+};
+
 export const createColumnDef = ({ fields }: { fields: any[] }) => {
   const columnDef = fields.slice().map((field, index) => {
     const { name, displayName, type, id } = field;
@@ -13,17 +17,14 @@ export const createColumnDef = ({ fields }: { fields: any[] }) => {
       id,
       header: <HeaderCell icon={fieldTypeToIconMap[type] as IconName} header={displayName} />,
       cell: (cellContext: CellContext<any, unknown>) => {
-        <CellComponent field={field} cellContext={cellContext} index={index} />;
+        return <CellComponent field={field} cellContext={cellContext} index={index} />;
       },
       accessorKey: name,
       field,
+      index,
       minSize: index === 0 ? 200 : 50,
     };
   });
 
   return columnDef;
-};
-
-export const fieldTypeToIconMap: Record<string, string> = {
-  SINGLE_LINE_TEXT: 'text',
 };
