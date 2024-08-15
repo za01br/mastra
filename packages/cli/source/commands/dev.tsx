93b78b1ba3148ff2d7b25b643e71858850ce2a9f
Sam Bhagwat <calcsam@gmail.com>
2024-08-15 00:34:23 -0700

    tabs to spaces

--- packages/cli/source/commands/dev.tsx
+++ packages/cli/source/commands/dev.tsx
@@ -4,62 +4,62 @@ import {Text} from 'ink';
 import {execa, ExecaError} from 'execa';
 
 async function startNextDevServer() {
-	function log(message: any, isError = false) {
-		const timestamp = new Date().toISOString();
-		const formattedMessage = `[${timestamp}] ${message}\n`;
+  function log(message: any, isError = false) {
+    const timestamp = new Date().toISOString();
+    const formattedMessage = `[${timestamp}] ${message}\n`;
 
-		if (isError) {
-			process.stderr.write(formattedMessage);
-		} else {
-			process.stdout.write(formattedMessage);
-		}
-	}
+    if (isError) {
+      process.stderr.write(formattedMessage);
+    } else {
+      process.stdout.write(formattedMessage);
+    }
+  }
 
-	log('Starting Next.js dev server...');
+  log('Starting Next.js dev server...');
 
-	try {
-		// TODO: get output re-colorized
-		// TODO: fix cwd so it works from project directory, not just from the cli directory
-		const nextServer = execa(
-			`PROJECT_DIRECTORY=${process.cwd()} npm run dev -- -p 3454`,
-			{
-				cwd: './node_modules/@arkw/admin',
-				all: true,
-				buffer: false,
-				env: process.env,
-				shell: true,
-			},
-		);
+  try {
+    // TODO: get output re-colorized
+    // TODO: fix cwd so it works from project directory, not just from the cli directory
+    const nextServer = execa(
+      `PROJECT_DIRECTORY=${process.cwd()} npm run dev -- -p 3454`,
+      {
+        cwd: './node_modules/@arkw/admin',
+        all: true,
+        buffer: false,
+        env: process.env,
+        shell: true,
+      },
+    );
 
-		nextServer.all.on('data', (data: any) => {
-			const output = data.toString().trim();
-			log(output);
+    nextServer.all.on('data', (data: any) => {
+      const output = data.toString().trim();
+      log(output);
 
-			if (output.includes('compiled successfully')) {
-				log('Next.js dev server is ready!');
-			}
-		});
+      if (output.includes('compiled successfully')) {
+        log('Next.js dev server is ready!');
+      }
+    });
 
-		process.on('SIGINT', async () => {
-			log('Stopping Next.js dev server...');
-			await nextServer.kill();
-			process.exit();
-		});
+    process.on('SIGINT', async () => {
+      log('Stopping Next.js dev server...');
+      await nextServer.kill();
+      process.exit();
+    });
 
-		await nextServer;
-	} catch (error: any) {
-		if (error instanceof ExecaError) {
-			console.log(error);
-		}
-		log(`Error: ${error.message}`, true);
-		if (error.stderr) {
-			log(`stderr: ${error.stderr}`, true);
-		}
-	} finally {
-	}
+    await nextServer;
+  } catch (error: any) {
+    if (error instanceof ExecaError) {
+      console.log(error);
+    }
+    log(`Error: ${error.message}`, true);
+    if (error.stderr) {
+      log(`stderr: ${error.stderr}`, true);
+    }
+  } finally {
+  }
 }
 
 export default function Dev() {
-	startNextDevServer().catch(console.error);
-	return <Text>This should open up the Next.js dev server on port 3456</Text>;
+  startNextDevServer().catch(console.error);
+  return <Text>This should open up the Next.js dev server on port 3456</Text>;
 }
--- packages/cli/source/commands/init.tsx
+++ packages/cli/source/commands/init.tsx
@@ -7,109 +7,109 @@ import {Text} from 'ink';
 import {execa, ExecaError} from 'execa';
 
 function init(configFilePath: string) {
-	try {
-		// Check to make sure a package.json file exists..
-		const packageJsonPath = path.join(process.cwd(), 'package.json');
-		if (!fs.pathExistsSync(packageJsonPath)) {
-			console.log('No package.json file found in the current directory');
-			return false;
-		}
+  try {
+    // Check to make sure a package.json file exists..
+    const packageJsonPath = path.join(process.cwd(), 'package.json');
+    if (!fs.pathExistsSync(packageJsonPath)) {
+      console.log('No package.json file found in the current directory');
+      return false;
+    }
 
-		// Check to make sure `@arkw/core` is installed.
-		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-		if (!packageJson.dependencies || !packageJson.dependencies['core']) {
-			console.log(
-				'Please install @arkw/core before running this command (npm install @arkw/core)',
-			);
-			return false;
-		}
+    // Check to make sure `@arkw/core` is installed.
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+    if (!packageJson.dependencies || !packageJson.dependencies['core']) {
+      console.log(
+        'Please install @arkw/core before running this command (npm install @arkw/core)',
+      );
+      return false;
+    }
 
-		// Check if a config file already exists.
-		// const configFilePath = path.join(process.cwd(), 'arkw.config.ts');
-		if (fs.pathExistsSync(configFilePath)) {
-			console.log(
-				'Arkwright configuration file (arkw.config.ts) already exists',
-			);
-			return false;
-		}
+    // Check if a config file already exists.
+    // const configFilePath = path.join(process.cwd(), 'arkw.config.ts');
+    if (fs.pathExistsSync(configFilePath)) {
+      console.log(
+        'Arkwright configuration file (arkw.config.ts) already exists',
+      );
+      return false;
+    }
 
-		const config = generateBoilerplateConfig();
-		fs.outputFileSync(configFilePath, config);
+    const config = generateBoilerplateConfig();
+    fs.outputFileSync(configFilePath, config);
 
-		return config;
-	} catch (err) {
-		console.error(err);
-		return false;
-	}
+    return config;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 async function migrate(createOnly = false) {
-	function log(message: any, isError = false) {
-		const timestamp = new Date().toISOString();
-		const formattedMessage = `[${timestamp}] ${message}\n`;
+  function log(message: any, isError = false) {
+    const timestamp = new Date().toISOString();
+    const formattedMessage = `[${timestamp}] ${message}\n`;
 
-		if (isError) {
-			process.stderr.write(formattedMessage);
-		} else {
-			process.stdout.write(formattedMessage);
-		}
-	}
-	console.log('Migrating database...');
-	try {
-		// TODO: get this working in project dir rather than cli dir
-		// TODO: prompt user for db URL or create sqllite db
+    if (isError) {
+      process.stderr.write(formattedMessage);
+    } else {
+      process.stdout.write(formattedMessage);
+    }
+  }
+  console.log('Migrating database...');
+  try {
+    // TODO: get this working in project dir rather than cli dir
+    // TODO: prompt user for db URL or create sqllite db
 
-		const INJECT_DB_URL = `FUTURE_DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:54322/arkwright?schema=public"`;
+    const INJECT_DB_URL = `FUTURE_DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:54322/arkwright?schema=public"`;
 
-		const PRISMA_BIN = `./node_modules/core/node_modules/prisma/node_modules/.bin`;
+    const PRISMA_BIN = `./node_modules/core/node_modules/prisma/node_modules/.bin`;
 
-		const PRISMA_SCHEMA = `node_modules/core/src/prisma/schema.prisma`;
+    const PRISMA_SCHEMA = `node_modules/core/src/prisma/schema.prisma`;
 
-		const CREATE_ONLY = createOnly ? `--create-only` : ``;
+    const CREATE_ONLY = createOnly ? `--create-only` : ``;
 
-		const migrateCommand = execa(
-			`${INJECT_DB_URL} ${PRISMA_BIN}/prisma migrate dev ${CREATE_ONLY} --schema=${PRISMA_SCHEMA} --name initial_migration`,
-			{
-				env: process.env,
-				shell: true,
-				all: true,
-			},
-		);
+    const migrateCommand = execa(
+      `${INJECT_DB_URL} ${PRISMA_BIN}/prisma migrate dev ${CREATE_ONLY} --schema=${PRISMA_SCHEMA} --name initial_migration`,
+      {
+        env: process.env,
+        shell: true,
+        all: true,
+      },
+    );
 
-		migrateCommand.all.on('data', (data: any) => {
-			const output = data.toString().trim();
-			log(output);
-		});
+    migrateCommand.all.on('data', (data: any) => {
+      const output = data.toString().trim();
+      log(output);
+    });
 
-		await migrateCommand;
-	} catch (error: any) {
-		if (error instanceof ExecaError) {
-			console.log(error);
-		}
-		log(`Error: ${error.message}`, true);
-		if (error.stderr) {
-			log(`stderr: ${error.stderr}`, true);
-		}
-	} finally {
-	}
+    await migrateCommand;
+  } catch (error: any) {
+    if (error instanceof ExecaError) {
+      console.log(error);
+    }
+    log(`Error: ${error.message}`, true);
+    if (error.stderr) {
+      log(`stderr: ${error.stderr}`, true);
+    }
+  } finally {
+  }
 }
 
 export default function Init() {
-	const config = init(path.join(process.cwd(), 'arkw.config.ts'));
+  const config = init(path.join(process.cwd(), 'arkw.config.ts'));
 
-	// TODO: merge output from migrate better with config file text results
-	migrate();
+  // TODO: merge output from migrate better with config file text results
+  migrate();
 
-	if (config) {
-		return <Text>Arkwright configuration file written to arkw.config.ts</Text>;
-	}
+  if (config) {
+    return <Text>Arkwright configuration file written to arkw.config.ts</Text>;
+  }
 
-	return <Text>An Error occurred</Text>;
+  return <Text>An Error occurred</Text>;
 }
 
 // TODO: Move this to core
 function generateBoilerplateConfig() {
-	return `
+  return `
 import { Config, createFramework } from 'core';
 import { MailchimpIntegration } from 'future-mailchimp'
 
