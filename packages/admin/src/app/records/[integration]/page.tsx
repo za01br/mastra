3749bf84e4f80c075d126fc05e23a6ff72620b40
Kenny <adelekekehinde06@gmail.com>
2024-08-15 17:36:17 +0100

    types update

--- packages/admin/src/app/records/[integration]/page.tsx
+++ packages/admin/src/app/records/[integration]/page.tsx
@@ -1,12 +1,5 @@
-import { getConfig } from '@/lib/get-configuration';
-
 import { ClientLayout } from './client-layout';
 
 export default async function Integration({ params }: { params: { integration: string } }) {
-  const config = await getConfig();
-  const integrationConfig = config.integrations.find(config => config.name.toLowerCase() === params.integration);
-  const fields = integrationConfig?.displayConfig.gridView.fields;
-  const data = integrationConfig?.displayConfig.gridView.data;
-
-  return <ClientLayout integration={params.integration} fields={fields!} data={data!} />;
+  return <ClientLayout integration={params.integration} fields={[]} data={[]} />;
 }
