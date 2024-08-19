1df6c5b335a2d38d2d34db6c6418609380cc215c
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2024-08-19 16:39:02 +0100

    read bluepring data from mock file

--- packages/admin/src/service/service.blueprintWriter.ts
+++ packages/admin/src/service/service.blueprintWriter.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+import { AutomationBlueprint, UpdateAutomationBlueprintDto } from 'core/src/workflows/types';
+import last from 'lodash/last';
+
 export class BlueprintWriterService {
   private directoryPath: string;
 
@@ -8,17 +11,21 @@ export class BlueprintWriterService {
     this.directoryPath = directoryPath;
   }
 
-  async readBlueprint(filePath: string): Promise<any> {
+  async readBlueprint(filePath: string): Promise<AutomationBlueprint> {
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) reject(err);
-        else resolve(JSON.parse(data));
+        else {
+          const jsonData = JSON.parse(data);
+          const blueprintId = last(filePath?.split('/'))!;
+          resolve({ ...jsonData, id: blueprintId });
+        }
       });
     });
   }
 
   // TODO: Fix Blueprint data type.
-  async writeBlueprint(filePath: string, data: any): Promise<void> {
+  async writeBlueprint(filePath: string, data: UpdateAutomationBlueprintDto): Promise<void> {
     return new Promise((resolve, reject) => {
       fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', err => {
         if (err) reject(err);
@@ -28,7 +35,7 @@ export class BlueprintWriterService {
   }
 
   // TODO: Fix Blueprint data type.
-  async getBlueprints(): Promise<{ data: any; filePath: string }[]> {
+  async getBlueprints(): Promise<AutomationBlueprint[]> {
     return new Promise((resolve, reject) => {
       fs.readdir(this.directoryPath, async (err, files) => {
         if (err) reject(err);
@@ -39,7 +46,8 @@ export class BlueprintWriterService {
             const filePath = path.join(this.directoryPath, file);
             const jsonData = await this.readBlueprint(filePath);
             if (jsonData.title) {
-              result.push({ data: jsonData, filePath });
+              const blueprintId = last(filePath?.split('/'))!;
+              result.push({ ...jsonData, id: blueprintId });
             }
           }
           resolve(result);
