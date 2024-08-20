2dd1fbee99b272ef0c9d4a4cc6ab4c3b91faf27a
Sam Bhagwat <calcsam@gmail.com>
2024-08-19 22:30:29 -0700

    get commander working

---
+++ packages/directory_manager/src/index.ts
@@ -0,0 +1,28 @@
+import { Command } from "commander";
+import { init } from './commands/init.js';
+import { dev } from './commands/dev.js';
+
+
+//add the following line
+const program = new Command();
+
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-l, --ls  [value]", "List directory contents")
+  .option("-m, --mkdir <value>", "Create a directory")
+  .option("-t, --touch <value>", "Create a file")
+
+
+program.command('init')
+  .description('Initialize a new project')
+  .action(() => init())
+
+program.command('dev')
+  .description('Start the development server')
+  .action(() => dev())
+
+program.parse(process.argv);
+
+
+const options = program.opts();
