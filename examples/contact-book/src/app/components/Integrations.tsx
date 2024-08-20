dc9a91a1ca46c7880c7baa00e77c0b990c655db2
Andrew Bettke <abettke@gmail.com>
2024-08-20 16:18:27 -0400

    FUTR-129: Add badge to button.

--- examples/contact-book/src/app/components/Integrations.tsx
+++ examples/contact-book/src/app/components/Integrations.tsx
@@ -1,16 +1,20 @@
-import { Button } from '@nextui-org/react';
+import { Button, Badge } from '@nextui-org/react';
 
 import Image from 'next/image';
 
 import arkw from '@/arkw.config';
 
 export const Integrations = () => {
+  // TODO: Need a way to query for connected integrations from arkw
+
   return (
     <div className="flex gap-4 items-center">
       {arkw.integrations.map(integration => (
-        <Button key={integration.name} isIconOnly variant={'faded'} aria-label="integration-name">
-          <Image src={integration.logoUrl} alt={''} width={48} height={48} />
-        </Button>
+        <Badge color={'danger'} content={' '} size={'sm'}>
+          <Button key={integration.name} isIconOnly variant={'faded'} aria-label="integration-name">
+            <Image src={integration.logoUrl} alt={''} width={48} height={48} />
+          </Button>
+        </Badge>
       ))}
     </div>
   );
