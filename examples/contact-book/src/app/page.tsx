e0c37c328d1a7176dcc48ebe6b08ed6a29d63112
Andrew Bettke <abettke@gmail.com>
2024-08-20 16:20:20 -0400

    FUTR-129: Rm unused imports.

--- examples/contact-book/src/app/page.tsx
+++ examples/contact-book/src/app/page.tsx
@@ -1,17 +1,13 @@
-import { Integration } from '@arkw/core';
 import { NextUIProvider } from '@nextui-org/react';
 
 import { Integrations } from '@/app/components/Integrations';
-import arkw from '@/arkw.config';
 
 export default function Home() {
-  const integrations = arkw.integrations;
-
   return (
     <main className={'h-full'}>
       <NextUIProvider>
         <div className={'flex justify-center p-4'}>
-          <Integrations available={integrations} connected={[]} />
+          <Integrations />
         </div>
       </NextUIProvider>
     </main>
