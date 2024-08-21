642432fe93044cb54a6092cb77756b2b0822b952
Kenny <adelekekehinde06@gmail.com>
2024-08-21 12:45:45 +0100

    feat: handle visibility

--- packages/admin/src/domains/records/components/columns/column-def.tsx
+++ packages/admin/src/domains/records/components/columns/column-def.tsx
@@ -1,3 +1,4 @@
+import { Property } from '@arkw/core';
 import { CellContext } from '@tanstack/react-table';
 
 import { IconName } from '@/types/icons';
@@ -9,22 +10,27 @@ export const propertyTypeToIconMap: Record<string, IconName> = {
   SINGLE_LINE_TEXT: 'text',
 };
 
-export const createColumnDef = ({ properties }: { properties: any[] }) => {
-  const columnDef = properties.slice().map((property, index) => {
-    const { name, displayName, type, id } = property;
+export const createColumnDef = ({ properties }: { properties: Property[] }) => {
+  console.log({ properties });
+  const columnDef = properties
+    .slice()
+    .filter(property => property.visible)
+    .sort((a, b) => a.order - b.order)
+    .map((property, index) => {
+      const { name, displayName, type, id } = property;
 
-    return {
-      id,
-      header: <HeaderCell icon={propertyTypeToIconMap[type] as IconName} header={displayName} />,
-      cell: (cellContext: CellContext<any, unknown>) => {
-        return <CellComponent property={property} cellContext={cellContext} index={index} />;
-      },
-      accessorKey: name,
-      property,
-      index,
-      minSize: index === 0 ? 200 : 50,
-    };
-  });
+      return {
+        id,
+        header: <HeaderCell icon={propertyTypeToIconMap[type] as IconName} header={displayName} />,
+        cell: (cellContext: CellContext<any, unknown>) => {
+          return <CellComponent property={property} cellContext={cellContext} index={index} />;
+        },
+        accessorKey: name,
+        property,
+        index,
+        minSize: index === 0 ? 200 : 50,
+      };
+    });
 
   return columnDef;
 };
