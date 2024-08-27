323c9e2f2123c8a2e5dbe92a28e9c9231f08b73e
Kenny <adelekekehinde06@gmail.com>
2024-08-27 17:43:54 +0100

    feat: extend tab

--- packages/admin/src/app/components/tab.tsx
+++ packages/admin/src/app/components/tab.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
@@ -10,38 +12,74 @@ export function Tab({
   url,
   isActive,
   icon,
-  isConnected = true,
+
+  as = 'link',
+  children,
+  classname,
 }: {
   text: string;
   url: string;
   isActive: boolean;
   icon: IconName;
-  isConnected?: boolean;
+
+  as?: 'link' | 'div';
+  children?: ReactNode;
+  classname?: string;
 }) {
-  return (
-    <Link
-      href={url}
-      className={cn(
-        'flex w-full  px-2 items-center gap-3 transition-all rounded-xs group text-small hover:bg-arkw-el-6/5',
-        isActive ? 'bg-arkw-el-6/5' : '',
-      )}
-    >
-      <Icon
-        name={icon}
+  if (as === 'link') {
+    return (
+      <Link
+        href={url}
         className={cn(
-          'w-[0.875rem] h-[0.875rem] text-arkw-el-3 group-hover:text-arkw-el-6',
-          isActive ? 'text-arkw-el-6' : '',
-        )}
-      />
-      <p
-        className={cn(
-          'py-[0.38rem] text-arkw-el-6/60 group-hover:text-arkw-el-6 transition-all  capitalize ',
-          isActive ? 'text-arkw-el-6' : '',
+          'flex w-full px-2 items-center gap-3 transition-all rounded-xs group text-small hover:bg-arkw-el-6/5',
+          isActive ? 'bg-arkw-el-6/5' : '',
+          classname,
         )}
       >
-        {text}
-      </p>
-      {isConnected ? '' : <Icon name="unplug" className="text-arkw-el-3 h-3 w-3 ml-auto" />}
-    </Link>
+        <Icon
+          name={icon}
+          className={cn(
+            'w-[0.875rem] h-[0.875rem] text-arkw-el-3 group-hover:text-arkw-el-6',
+            isActive ? 'text-arkw-el-6' : '',
+          )}
+        />
+        <p
+          className={cn(
+            'py-[0.38rem] text-arkw-el-6/60 group-hover:text-arkw-el-6 transition-all  capitalize ',
+            isActive ? 'text-arkw-el-6' : '',
+          )}
+        >
+          {text}
+        </p>
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      type="button"
+      className={cn(
+        'flex w-full cursor-pointer px-2 items-center justify-between transition-all rounded-xs text-small hover:bg-arkw-el-6/5',
+      )}
+    >
+      <div className="flex group gap-3 items-center">
+        <Icon
+          name={icon}
+          className={cn(
+            'w-[0.875rem] h-[0.875rem] text-arkw-el-3 group-hover:text-arkw-el-6',
+            isActive ? 'text-arkw-el-6' : '',
+          )}
+        />
+        <p
+          className={cn(
+            'py-[0.38rem] text-arkw-el-6/60 select-none group-hover:text-arkw-el-6 transition-all  capitalize ',
+            isActive ? 'text-arkw-el-6' : '',
+          )}
+        >
+          {text}
+        </p>
+      </div>
+      {children}
+    </button>
   );
 }
