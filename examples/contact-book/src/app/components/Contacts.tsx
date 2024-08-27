b47682c5231b8d26a8125dc6310d08990b3210ff
Andrew Bettke <abettke@gmail.com>
2024-08-27 14:08:45 -0400

    FUTR-129: WIP querying contacts.

--- examples/contact-book/src/app/components/Contacts.tsx
+++ examples/contact-book/src/app/components/Contacts.tsx
@@ -1,10 +1,23 @@
+import { default as arkw } from '@arkw/config';
 import { Divider, Card, CardBody, CardHeader } from '@nextui-org/react';
 
-import arkw from '@/arkw.config';
+import { getSession } from '@/app/actions/session';
 
-export const Contacts = () => {
-  // TODO: Get records from framework
-  // const records = arkw.dataLayer.getRecords({ entity: 'Contact' });
+export const Contacts = async () => {
+  const sessionId = (await getSession())!;
+  const integrations = await arkw.connectedIntegrations({
+    context: {
+      referenceId: sessionId,
+    },
+  });
+
+  const contacts = integrations.map(integration => {
+    return integration.query({
+      referenceId: sessionId,
+      entityType: 'contact',
+    });
+  });
+
   return (
     <div className={'m-4 space-x-0'}>
       <Divider className={'m-4'} />
