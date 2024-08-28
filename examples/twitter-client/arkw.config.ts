b94f75d2120498674ecd4e95f158dcd35b47ec78
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2024-08-28 16:30:23 +0100

    add x integration to twitter-client

--- examples/twitter-client/arkw.config.ts
+++ examples/twitter-client/arkw.config.ts
@@ -1,11 +1,20 @@
 import { Config } from '@arkw/core';
+import { XIntegration } from '@arkw/x';
 
 export const config: Config = {
   name: 'twitter-client',
   //logConfig: {}, // TODO: Add this
   systemActions: [],
   systemEvents: {},
-  integrations: [],
+  integrations: [
+    new XIntegration({
+      config: {
+        CLIENT_ID: process.env.X_CLIENT_ID!,
+        CLIENT_SECRET: process.env.X_CLIENT_SECRET!,
+        REDIRECT_URI: new URL('/api/arkw/connect/callback', process.env.APP_URL).toString(),
+      },
+    }),
+  ],
   db: {
     provider: 'postgres',
     uri: 'postgresql://postgres:postgres@127.0.0.1:5433/arkwright?schema=arkw',
