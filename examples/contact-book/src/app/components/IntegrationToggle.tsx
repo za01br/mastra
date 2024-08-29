44722178326d1c883b0ad479bc5b8e42ca37593f
Andrew Bettke <abettke@gmail.com>
2024-08-29 12:01:19 -0400

    FIX: Broken oauth flow. Build connect URL using framework method now.

--- examples/contact-book/src/app/components/IntegrationToggle.tsx
+++ examples/contact-book/src/app/components/IntegrationToggle.tsx
@@ -1,33 +1,20 @@
 'use client';
 
-import { Badge, Button } from '@nextui-org/react';
+import { Badge, Button, Link } from '@nextui-org/react';
 
 import { default as Image } from 'next/image';
 
 type IntegrationToggleProps = {
   name: string;
   logoUrl: string;
-  connectionId: string;
   connectUrl: string;
   connected: boolean;
 };
 
 export const IntegrationToggle = (props: IntegrationToggleProps) => {
-  const onConnect = () => {
-    const connectUrl = new URL(props.connectUrl, window.location.href);
-    const connectParams = new URLSearchParams({
-      name: props.name,
-      connectionId: props.connectionId,
-    });
-
-    connectUrl.search = connectParams.toString();
-
-    window.location.assign(connectUrl.toString());
-  };
-
   return (
     <Badge key={props.name} color={props.connected ? 'success' : 'danger'} content={' '} size={'sm'}>
-      <Button isIconOnly variant={'faded'} aria-label="integration-name" onClick={onConnect}>
+      <Button isIconOnly as={Link} variant={'faded'} aria-label="integration-name" href={props.connectUrl}>
         <Image src={props.logoUrl} alt={''} width={48} height={48} />
       </Button>
     </Badge>
