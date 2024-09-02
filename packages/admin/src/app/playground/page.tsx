2b832bd18711e3f0d19bd214b9bac9cbcc831b49
Kenny <adelekekehinde06@gmail.com>
2024-09-02 16:10:37 +0100

    feat: show api account

--- packages/admin/src/app/playground/page.tsx
+++ packages/admin/src/app/playground/page.tsx
@@ -1,6 +1,6 @@
 import { framework } from '@/lib/framework-utils';
 
-import { ConnectionList } from './components/connection-list';
+import { Connection } from './components/connection-list';
 
 async function Playground() {
   const connections = (await framework?.dataLayer.getAllConnections()) || [];
@@ -41,13 +41,20 @@ async function Playground() {
 
   return (
     <section>
-      <h1 className="text-sm gradient capitalize border-b-[0.5px] py-2 border-primary-border p-4">Playground</h1>
-      <div className="p-4 flex flex-col gap-4 mx-auto max-w-[40em]">
+      <h1 className="text-sm relative gradient capitalize border-b-[0.5px] py-2 border-primary-border p-4">
+        Playground
+      </h1>
+      <div className="p-4 flex  flex-col gap-4 mx-auto max-w-[40em]">
         <div className="">
           <h1 className="text-xl">Connected Integrations</h1>
           <p className="text-sm text-arkw-el-3">Explore events and apis for connected integrations</p>
         </div>
-        <ConnectionList connections={uniqueConnections} />
+        <div className="grid grid-cols-4 max-w-72 gap-x-5 gap-3">
+          {uniqueConnections.map(connection => {
+            const apiCount = Object.keys(connection.apis)?.length;
+            return <Connection key={connection.referenceId} name={connection.name} apiCount={apiCount} />;
+          })}
+        </div>
       </div>
     </section>
   );
