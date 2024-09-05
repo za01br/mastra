54fbb898bc140c2993f6f607ec48b6c22bb25c97
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2024-09-05 17:35:38 +0100

    update framework methods in twitter client

--- examples/twitter-client/arkw.config.ts
+++ examples/twitter-client/arkw.config.ts
@@ -6,19 +6,13 @@ import z from 'zod';
 export const config: Config = {
   name: 'twitter-client',
   //logConfig: {}, // TODO: Add this
-  systemActions: [],
+  systemApis: [],
   systemEvents: {
     BUTTON_CLICKED: {
       schema: z.object({}),
-      triggerProperties: {
-        type: 'BUTTON_CLICKED',
-        label: 'Button Clicked',
-        icon: {
-          alt: 'Button Clicked',
-          icon: 'dashboard',
-        },
-        description: 'Triggered when trigger button is clicked',
-      },
+      key: 'BUTTON_CLICKED',
+      label: 'Button Clicked',
+      description: 'Triggered when trigger button is clicked',
     },
   },
   integrations: [
@@ -43,7 +37,7 @@ export const config: Config = {
   ],
   db: {
     provider: 'postgres',
-    uri: 'postgresql://postgres:postgres@127.0.0.1:5433/arkwright?schema=arkw',
+    uri: 'postgresql://postgres:postgres@127.0.0.1:5432/arkwright?schema=arkw',
   },
   systemHostURL: process.env.APP_URL!,
   routeRegistrationPath: '/api/arkw',
