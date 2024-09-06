e197ae0da8a7e8c35275971c560ba4e04f048473
Kenny <adelekekehinde06@gmail.com>
2024-09-06 12:31:51 +0100

    feat: update button implementation detail

--- packages/admin/src/app/(dashboard)/playground/components/run-button.tsx
+++ packages/admin/src/app/(dashboard)/playground/components/run-button.tsx
@@ -2,14 +2,16 @@
 
 import { Icon } from '@/app/components/icon';
 
-function RunApiOrEvent({ context }: { context: 'api' | 'event' }) {
+function RunApiOrEvent({ context, onClick }: { context: 'api' | 'event'; onClick?: () => void }) {
   return (
     <button
-      onClick={() => alert('event ran')}
-      className="bg-[#33616B]  border-[0.5px] border-[#5699A8] rounded text-sm font-medium text-arkw-el-5 w-[10.25rem] py-2 px-3"
+      onClick={onClick}
+      className="justify-center bg-[#33616B] z-30 absolute inset-0 rounded flex items-center gap-1 text-sm font-medium text-arkw-el-5"
     >
       <Icon name="activity" />
-      Run {context}
+      <span>
+        Run <span className="capitalize">{context}</span>
+      </span>
     </button>
   );
 }
