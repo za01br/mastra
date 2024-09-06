8c4d7888c123c15bddc7ac1ddd281ff7c9c53596
Kenny <adelekekehinde06@gmail.com>
2024-09-06 10:24:24 +0100

    feat: pass props and render proper comoponents

--- packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
+++ packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
@@ -1,24 +1,23 @@
-'use client';
-
 import { Separator } from '@radix-ui/react-dropdown-menu';
 
+import { ApiCodeBlock } from '@/domains/playground/components/api-code-block';
+
+import { ApiSchemaBlock } from '../../components/api/api-schema-block';
+import { RunApiOrEvent } from '../../components/run-button';
+
 export default function Page({ params }: { params: { api: Array<string> } }) {
-  const [integrationName, api] = params.api;
+  const [_, api] = params.api;
+  // use the send_email to get the action detail from a list
   return (
     <div className="grid h-[calc(100%-1.24rem)] gap-x-[0.62rem] grid-cols-[23.5rem_0.5px_1fr]">
-      <div className="border-[0.5px] rounded-[0.375rem] bg-arkw-bg-2 border-arkw-border-1 p-2">some stuff</div>
+      <ApiSchemaBlock type={api.toUpperCase()} />
       <Separator className="w-[0.5px] bg-arkw-border-1" />
       <div className="flex flex-col gap-5 rounded-[0.375rem]">
         <div className="basis-[27rem] border-[0.5px] border-arkw-border-1 p-2 rounded-[0.25rem] bg-arkw-bg-2">
-          another stuff
+          <ApiCodeBlock />
         </div>
         <div className="p-2 rounded-xl mx-auto border-arkw-border-2 border-[0.5px] w-fit">
-          <button
-            onClick={() => alert('event ran')}
-            className="bg-[#33616B]  border-[0.5px] border-[#5699A8] rounded text-sm font-medium text-arkw-el-5 w-[10.25rem] py-2 px-3"
-          >
-            Run event
-          </button>
+          <RunApiOrEvent context="api" />
         </div>
         <div className="flex-1 p-2  border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2">
           another stuff
