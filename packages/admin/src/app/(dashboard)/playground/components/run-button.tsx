2f31d14bfd9e89ad7a35bac0f7529ff058adaf0b
Kenny <adelekekehinde06@gmail.com>
2024-09-08 12:52:37 +0100

    feat: handle loading state

--- packages/admin/src/app/(dashboard)/playground/components/run-button.tsx
+++ packages/admin/src/app/(dashboard)/playground/components/run-button.tsx
@@ -1,18 +1,36 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+
 import { Icon } from '@/app/components/icon';
 
-function RunApiOrEvent({ context, onClick }: { context: 'api' | 'event'; onClick?: () => void }) {
+function RunApiOrEvent({
+  context,
+  onClick,
+  apiIsRunning,
+}: {
+  context: 'api' | 'event';
+  onClick?: () => void;
+  apiIsRunning?: boolean;
+}) {
   return (
-    <button
+    <Button
+      variant={'ghost'}
       onClick={onClick}
+      disabled={apiIsRunning}
       className="justify-center bg-[#33616B] absolute inset-0 rounded flex items-center gap-1 text-sm font-medium text-arkw-el-5"
     >
       <Icon name="activity" />
-      <span>
-        Run <span className="capitalize">{context}</span>
-      </span>
-    </button>
+      {apiIsRunning ? (
+        <span>
+          Running <span className="capitalize">{context}...</span>
+        </span>
+      ) : (
+        <span>
+          Run <span className="capitalize">{context}</span>
+        </span>
+      )}
+    </Button>
   );
 }
 
