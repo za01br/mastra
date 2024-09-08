b17ac06d1110e89e1b0943790c38997c9ea57c94
Kenny <adelekekehinde06@gmail.com>
2024-09-08 12:54:18 +0100

    feat: improve code block with descriptive section

--- packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
+++ packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
@@ -2,22 +2,26 @@ import { Separator } from '@radix-ui/react-dropdown-menu';
 
 import { ApiCodeBlock } from '@/domains/playground/components/api-code-block';
 
+import { ApiResultContainer } from '../../components/api/api-result-container';
 import { ApiSchemaBlock } from '../../components/api/api-schema-block';
 import { RunButtonContainer } from '../../components/api/run-button-container';
 
 export default function Page({ params }: { params: { api: Array<string> } }) {
   const [_, api] = params.api;
-  // use the send_email to get the action detail from a list
+
   return (
     <div className="grid h-[calc(100%-1.24rem)] gap-x-[0.62rem] grid-cols-[23.5rem_0.5px_1fr]">
       <ApiSchemaBlock type={api.toUpperCase()} />
       <Separator className="w-[0.5px] bg-arkw-border-1" />
       <div className="flex flex-col gap-5 rounded-[0.375rem]">
-        <div className="basis-[27rem] border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2">
+        <div className="basis-[27rem] grow-0 relative z-20 border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2">
           <ApiCodeBlock />
+          <div className="absolute left-0 text-sm rounded-bl-[0.25rem] grid place-items-center rounded-br-[0.25rem] bottom-0 w-full text-center text-arkw-el-3 bg-arkw-bg-13 h-10 py-1 px-4">
+            Code block to use in your app
+          </div>
         </div>
         <RunButtonContainer />
-        <div className="flex-1 p-2  border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2">Api Result</div>
+        <ApiResultContainer />
       </div>
     </div>
   );
