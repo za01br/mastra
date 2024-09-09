e4cc32b395fc61bcd36c79578c309ea5ec0fcd5d
dayo <ifedayoadeniyi@gmail.com>
2024-09-09 15:54:18 +0100

    cleanup

--- packages/twilio/src/Twilio.test.ts
+++ packages/twilio/src/Twilio.test.ts
@@ -1,93 +1,88 @@
-import { createFramework, EventHandlerExecutorParams } from '@arkw/core';
-import { describe, expect, it } from '@jest/globals';
-import {
-  ZodSchema,
-  ZodObject,
-  ZodString,
-  ZodNumber,
-  ZodBoolean,
-  ZodArray,
-  ZodEnum,
-  ZodOptional,
-  ZodUnion,
-  ZodLiteral,
-} from 'zod';
 
-import { TwilioIntegration } from '.';
+          import { describe, expect, it } from '@jest/globals';
+          import {createFramework, EventHandlerExecutorParams} from '@arkw/core';
+          import {TwilioIntegration} from '.'
+          import { ZodSchema, ZodObject, ZodString, ZodNumber, ZodBoolean, ZodArray, ZodEnum, ZodOptional, ZodUnion, ZodLiteral} from 'zod';
 
-const ACCOUNT_SID = 'AC5ec84f282f40c877009d05409b31df75';
-const AUTH_TOKEN = '356fac71cc9fd3490e8789b753c97f26';
-const dbUri = 'postgresql://postgres:postgres@localhost:5432/arkwright?schema=arkw';
-const referenceId = '1';
 
-const integrationName = 'TWILIO';
+          const ACCOUNT_SID = '';
+const AUTH_TOKEN = '';
+          const dbUri = 'postgresql://postgres:postgres@localhost:5432/arkwright?schema=arkw';
+          const referenceId = '1'
 
-const integrationFramework = createFramework({
-  name: 'TestFramework',
-  integrations: [new TwilioIntegration()],
-  systemApis: [],
-  systemEvents: {},
-  db: {
-    provider: 'postgres',
-    uri: dbUri,
-  },
-  systemHostURL: 'http://localhost:3000',
-  routeRegistrationPath: '/api/arkw',
-  blueprintDirPath: '',
-});
+          const integrationName = 'TWILIO'
 
-const integration = integrationFramework.getIntegration(integrationName);
-const integrationEvents = integrationFramework.getEventsByIntegration(integrationName);
-const integrationAPIs = integrationFramework.getApisByIntegration(integrationName);
+          const integrationFramework = createFramework({
+          name: 'TestFramework',
+          integrations: [
+            new TwilioIntegration(),
+          ],
+          systemApis: [],
+          systemEvents: {},
+          db: {
+            provider: 'postgres',
+            uri: dbUri,
+          },
+          systemHostURL: 'http://localhost:3000',
+          routeRegistrationPath: '/api/arkw',
+          blueprintDirPath: '',
+        });
 
-function generateMockData(schema: ZodSchema<any>): any {
-  if (schema instanceof ZodObject) {
-    const shape = schema.shape;
-    const mockObject: Record<string, any> = {};
-    for (const key in shape) {
-      mockObject[key] = generateMockData(shape[key]);
-    }
-    return mockObject;
-  }
+        const integration = integrationFramework.getIntegration(integrationName);
+        const integrationEvents = integrationFramework.getEventsByIntegration(integrationName);
+        const integrationAPIs = integrationFramework.getApisByIntegration(integrationName);
 
-  if (schema instanceof ZodString) {
-    return '1208172064188957';
-  }
+        function generateMockData(schema: ZodSchema<any>): any {
+          if (schema instanceof ZodObject) {
+              const shape = schema.shape;
+              const mockObject: Record<string, any> = {};
+              for (const key in shape) {
+                  mockObject[key] = generateMockData(shape[key]);
+              }
+              return mockObject;
+          }
 
-  if (schema instanceof ZodNumber) {
-    return 1208172064188957;
-  }
+          if (schema instanceof ZodString) {
+              return "1208172064188957";
+          }
 
-  if (schema instanceof ZodBoolean) {
-    return true;
-  }
+          if (schema instanceof ZodNumber) {
+              return 1208172064188957;
+          }
 
-  if (schema instanceof ZodArray) {
-    const elementSchema = schema.element;
-    return [generateMockData(elementSchema)];
-  }
+          if (schema instanceof ZodBoolean) {
+              return true;
+          }
 
-  if (schema instanceof ZodEnum) {
-    return schema.options[0];
-  }
+          if (schema instanceof ZodArray) {
+              const elementSchema = schema.element;
+              return [generateMockData(elementSchema)];
+          }
 
-  if (schema instanceof ZodOptional) {
-    return generateMockData(schema.unwrap());
-  }
+          if (schema instanceof ZodEnum) {
+              return schema.options[0];
+          }
 
-  if (schema instanceof ZodUnion) {
-    return generateMockData(schema.options[0]);
-  }
+          if (schema instanceof ZodOptional) {
+              return generateMockData(schema.unwrap());
+          }
 
-  if (schema instanceof ZodLiteral) {
-    return schema.value;
-  }
+          if (schema instanceof ZodUnion) {
+              return generateMockData(schema.options[0]);
+          }
 
-  return {};
-}
+          if (schema instanceof ZodLiteral) {
+              return schema.value;
+          }
 
-describe('twilio', () => {
-  beforeAll(async () => {
+          return {}
+        }
+
+
+      describe('twilio', () => {
+
+        beforeAll(async () => {
+          
     await integrationFramework.connectIntegrationByCredential({
       name: integrationName,
       referenceId,
@@ -98,15 +93,17 @@ describe('twilio', () => {
         },
         type: 'API_KEY',
       },
-    });
-  });
+    })
+    
+        })
 
-  describe('events', () => {
-    it('should have events', () => {
-      expect(integrationEvents).toBeDefined();
-    });
+       describe('events', () => {
 
-    for (const event of Object.entries(integrationEvents ?? {})) {
+         it('should have events', () => {
+          expect(integrationEvents).toBeDefined();
+        });
+
+        for (const event of Object.entries(integrationEvents ?? {})) {
       const [key, value] = event;
 
       it(`should send event: ${key}`, async () => {
@@ -146,61 +143,64 @@ describe('twilio', () => {
           return;
         }
 
-        await handler({
-          eventKey: key,
-          integrationInstance: integration,
-          makeWebhookUrl: integrationFramework.makeWebhookUrl,
-        }).executor({
-          event: {
-            data: generateMockData(schema as ZodSchema<any>),
-            user: {
-              referenceId,
-            },
-            name: integrationName,
-          },
-          step: {} as unknown as EventHandlerExecutorParams['step'],
-          attempt: 1,
-          events: [
-            {
-              name: 'event',
-            },
-          ],
-          runId: '1',
+            await handler({
+              eventKey: key,
+              integrationInstance: integration,
+              makeWebhookUrl: integrationFramework.makeWebhookUrl,
+            }).executor({
+              event: {
+                data: generateMockData(schema as ZodSchema<any>),
+                user: {
+                  referenceId,
+                },
+                name: integrationName,
+              },
+              step: {} as unknown as EventHandlerExecutorParams['step'],
+              attempt: 1,
+              events: [
+                {
+                  name: 'event',
+                },
+              ],
+              runId: '1',
+            });
+
+            // expect(response.status).toBe(200);
+          });
+        }
+       })
+
+       describe('apis', () => {
+         it('should have APIs', () => {
+          expect(integrationAPIs).toBeDefined();
         });
 
-        // expect(response.status).toBe(200);
-      });
-    }
-  });
+        for (const api of Object.values(integrationAPIs ?? {})) {
+          it(`should hit APIs: ${api.type}`, async () => {
+            const data = generateMockData(api.schema as ZodSchema<any>);
+            await integrationFramework.executeApi({
+              integrationName,
+              api: api.type,
+              payload: {
+                ctx: {
+                  referenceId,
+                },
+                data,
+              },
+            });
 
-  describe('apis', () => {
-    it('should have APIs', () => {
-      expect(integrationAPIs).toBeDefined();
-    });
+            // expect(response.status).toBe(200);
+          });
+        }
+       })
 
-    for (const api of Object.values(integrationAPIs ?? {})) {
-      it(`should hit APIs: ${api.type}`, async () => {
-        const data = generateMockData(api.schema as ZodSchema<any>);
-        await integrationFramework.executeApi({
-          integrationName,
-          api: api.type,
-          payload: {
-            ctx: {
-              referenceId,
-            },
-            data,
-          },
-        });
-
-        // expect(response.status).toBe(200);
-      });
-    }
-  });
-
-  afterAll(async () => {
+       afterAll(async()=>{
+          
     await integrationFramework.disconnectIntegration({
       name: integrationName,
       referenceId,
     });
-  });
-});
+  
+       })
+      })
+     
