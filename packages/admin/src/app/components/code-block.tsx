389c2739733f856adb60c960e56ea2189fc9ae50
Kenny <adelekekehinde06@gmail.com>
2024-09-10 12:38:36 +0100

    fix: remove shadow from codeblock

--- packages/admin/src/app/components/code-block.tsx
+++ packages/admin/src/app/components/code-block.tsx
@@ -3,7 +3,7 @@ import { CodeBlock } from 'react-code-block';
 function CodeBlockDemo({ code = "console.log('Hello World!')" }: { code?: string }) {
   return (
     <CodeBlock code={code} language="ts">
-      <CodeBlock.Code className="bg-transparent h-full p-6 rounded-xl shadow-lg">
+      <CodeBlock.Code className="bg-transparent h-full p-6 rounded-xl">
         <div className="table-row">
           <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
           <CodeBlock.LineContent className="table-cell">
