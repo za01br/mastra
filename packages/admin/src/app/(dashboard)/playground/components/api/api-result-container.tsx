ea2e9d14cb50c5f1fa26ccedf0ecfac7a976b271
Kenny <adelekekehinde06@gmail.com>
2024-09-10 12:50:13 +0100

    feat: show type of response

--- packages/admin/src/app/(dashboard)/playground/components/api/api-result-container.tsx
+++ packages/admin/src/app/(dashboard)/playground/components/api/api-result-container.tsx
@@ -11,8 +11,9 @@ export function ApiResultContainer() {
       id="api-result-container"
       className="flex-1 relative z-20 border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2"
     >
-      <div className="absolute text-sm rounded-tl-[0.25rem] grid place-items-center rounded-tr-[0.25rem] top-0 w-full text-center text-arkw-el-3 bg-arkw-bg-13 h-10 py-1 px-4">
-        This is the Output
+      <div className="absolute flex justify-between items-center text-sm rounded-tl-[0.25rem] rounded-tr-[0.25rem] top-0 w-full text-center text-arkw-el-3 bg-arkw-bg-13 h-10 py-1 px-4">
+        <p className="text-sm">Output</p>
+        <p className="text-xs border border-arkw-border-1 p-1 rounded bg-arkw-bg-8 font-medium">JSON</p>
       </div>
       <div className="p-2 pt-12">
         {apiRunState === 'success' ? (
