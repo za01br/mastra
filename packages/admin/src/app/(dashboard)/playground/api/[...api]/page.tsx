25f1c69c811dd6883da00e6a03988a195ad9835d
Kenny <adelekekehinde06@gmail.com>
2024-09-11 17:07:00 +0100

    feat: move container to parent

--- packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
+++ packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
@@ -21,7 +21,16 @@ export default function Page({ params }: { params: { api: Array<string> } }) {
           </div>
         </div>
         <RunButtonContainer />
-        <ApiResultContainer />
+        <div
+          id="api-result-container"
+          className="relative flex-1 z-20 border-[0.5px] border-arkw-border-1 rounded-[0.25rem] bg-arkw-bg-2"
+        >
+          <div className="absolute flex justify-between items-center text-sm rounded-tl-[0.25rem] rounded-tr-[0.25rem] top-0 w-full text-center text-arkw-el-3 bg-arkw-bg-13 h-10 py-1 px-4">
+            <p className="text-sm">Output</p>
+            <p className="text-xs border border-arkw-border-1 p-1 rounded bg-arkw-bg-8 font-medium">JSON</p>
+          </div>
+          <ApiResultContainer />
+        </div>
       </div>
     </div>
   );
