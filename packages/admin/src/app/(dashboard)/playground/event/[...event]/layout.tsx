38280b5082da46cf6a6a3f1cb6ef10222c8efa42
Kenny <adelekekehinde06@gmail.com>
2024-09-12 13:37:09 +0100

    feat: remove unused param

--- packages/admin/src/app/(dashboard)/playground/event/[...event]/layout.tsx
+++ packages/admin/src/app/(dashboard)/playground/event/[...event]/layout.tsx
@@ -1,17 +1,14 @@
 import { RefinedIntegrationEvent } from '@arkw/core';
 import { ReactNode } from 'react';
 
-import Breadcrumb from '@/components/ui/breadcrumbs';
-
 import { framework } from '@/lib/framework-utils';
-import { toTitleCase } from '@/lib/string';
 
 import { EventPlaygroundProvider } from '@/domains/playground/providers/event-playground-provider';
 import { getSerializedFrameworkEvents } from '@/domains/workflows/utils';
 
-export default async function Layout({ params, children }: { children: ReactNode; params: { event: Array<string> } }) {
-  const [_, event] = params.event;
+import { PlaygroundBreadCrumb } from '../../components/playground-breadcrumb';
 
+export default async function Layout({ children }: { children: ReactNode }) {
   const systemEvents = framework?.getSystemEvents();
 
   const availableIntegrations = framework?.availableIntegrations()?.map(({ integration }) => integration) || [];
@@ -47,21 +44,7 @@ export default async function Layout({ params, children }: { children: ReactNode
   return (
     <div className="overflow-hidden">
       <nav className="text-sm h-fit capitalize border-b-[0.5px] py-2 border-arkw-border-1 p-4">
-        <Breadcrumb
-          items={[
-            {
-              label: 'Playground',
-              href: `/playground`,
-              isCurrent: false,
-            },
-            {
-              label: toTitleCase(event, '_'),
-              href: ` `,
-              isCurrent: true,
-            },
-          ]}
-          pageClassName="whitespace-nowrap"
-        />
+        <PlaygroundBreadCrumb />
       </nav>
       <EventPlaygroundProvider serializedFrameworkEvents={serializedFrameworkEvents}>
         <section className="p-[0.62rem] bg-arkw-bg-1 h-[calc(100%-1.24rem)]">{children}</section>
