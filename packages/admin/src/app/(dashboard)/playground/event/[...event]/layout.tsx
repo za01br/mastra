b338e3a78cbe32c7a8606c5b7f5e3dcc236700a9
Kenny <adelekekehinde06@gmail.com>
2024-09-12 11:17:41 +0100

    feat: init file for event section

---
+++ packages/admin/src/app/(dashboard)/playground/event/[...event]/layout.tsx
@@ -0,0 +1,71 @@
+import { RefinedIntegrationEvent } from '@arkw/core';
+import { ReactNode } from 'react';
+
+import Breadcrumb from '@/components/ui/breadcrumbs';
+
+import { framework } from '@/lib/framework-utils';
+import { toTitleCase } from '@/lib/string';
+
+import { EventPlaygroundProvider } from '@/domains/playground/providers/event-playground-provider';
+import { getSerializedFrameworkEvents } from '@/domains/workflows/utils';
+
+export default async function Layout({ params, children }: { children: ReactNode; params: { event: Array<string> } }) {
+  const [_, event] = params.event;
+
+  const systemEvents = framework?.getSystemEvents();
+
+  const availableIntegrations = framework?.availableIntegrations()?.map(({ integration }) => integration) || [];
+
+  const availableIntegrationsEvents = availableIntegrations.reduce<RefinedIntegrationEvent[]>((acc, { name }) => {
+    const events = framework?.getEventsByIntegration(name) ?? {};
+    const refinedEvents: RefinedIntegrationEvent[] = Object.entries(events).map(([k, v]) => {
+      return {
+        ...v,
+        key: k,
+        label: k,
+        integrationName: name,
+      };
+    });
+    return [...acc, ...refinedEvents];
+  }, []);
+
+  const refinedSystemEvents: RefinedIntegrationEvent[] = Object.entries(systemEvents ?? {}).map(([k, v]) => {
+    return {
+      ...v,
+      key: k,
+      integrationName: framework?.config.name,
+    };
+  });
+
+  const frameworkEvents = [...refinedSystemEvents, ...availableIntegrationsEvents];
+
+  const serializedFrameworkEvents = await getSerializedFrameworkEvents({
+    frameworkEvents,
+    ctx: { referenceId: '' },
+  });
+
+  return (
+    <div className="overflow-hidden">
+      <nav className="text-sm h-fit capitalize border-b-[0.5px] py-2 border-arkw-border-1 p-4">
+        <Breadcrumb
+          items={[
+            {
+              label: 'Playground',
+              href: `/playground`,
+              isCurrent: false,
+            },
+            {
+              label: toTitleCase(event, '_'),
+              href: ` `,
+              isCurrent: true,
+            },
+          ]}
+          pageClassName="whitespace-nowrap"
+        />
+      </nav>
+      <EventPlaygroundProvider serializedFrameworkEvents={serializedFrameworkEvents}>
+        <section className="p-[0.62rem] bg-arkw-bg-1 h-[calc(100%-1.24rem)]">{children}</section>
+      </EventPlaygroundProvider>
+    </div>
+  );
+}
