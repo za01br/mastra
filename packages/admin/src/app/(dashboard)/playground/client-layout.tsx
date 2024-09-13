991d79194f1688c3d866e48f007a4f328d4b40b7
Kenny <adelekekehinde06@gmail.com>
2024-09-13 11:02:39 +0100

    feat: add system trigger

--- packages/admin/src/app/(dashboard)/playground/client-layout.tsx
+++ packages/admin/src/app/(dashboard)/playground/client-layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { TabsContent, TabsList } from '@radix-ui/react-tabs';
+import { TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
 import { useState } from 'react';
 
 import Link from 'next/link';
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { Tabs } from '@/components/ui/tabs';
 
 import { lowerCaseWord } from '@/lib/string';
+import { cn } from '@/lib/utils';
 
 import { Icon } from '@/app/components/icon';
 import { IconName } from '@/types/icons';
@@ -28,7 +29,7 @@ interface ClientLayoutProps {
 }
 export const ClientLayout = ({ connectedIntegrations }: ClientLayoutProps) => {
   const [integrationPlaygroundItem, setIntegrationPlaygroundItem] = useState({
-    name: lowerCaseWord(connectedIntegrations[0]?.name || ''),
+    name: 'system',
   });
 
   const updateCurrentIntegrationItem = (name: string) => {
@@ -47,37 +48,44 @@ export const ClientLayout = ({ connectedIntegrations }: ClientLayoutProps) => {
       <>
         <div className="border-r-[0.5px] border-r-kpl-border-1">
           <h1 className="text-sm gradient sticky top-0 bg-kpl-bg-3 capitalize border-b-kpl-border-1 border-b-[0.5px] py-3 p-4">
-            Connected Integrations
+            Playground
           </h1>
+
           <TabsList asChild>
-            <div className="flex px-3 py-2 flex-col gap-2 overflow-y-scroll">
-              {connectedIntegrations.map(integration => {
-                return (
-                  <ConnectedIntegration
-                    isActive={currentIntegrationName === lowerCaseWord(integration.name)}
-                    updateCurrentIntegration={updateCurrentIntegrationItem}
-                    key={integration.name}
-                    name={integration.name}
-                    connections={integration.connections}
-                    icon={integration.icon}
-                  />
-                );
-              })}
-              <Link
-                href="/integrations"
-                className="flex px-2 gap-3 py-2 group hover:bg-kpl-bg-3 transition-all items-center w-full"
-              >
-                <span className="w-[2.25rem] grid place-items-center h-[2.25rem] rounded-xs bg-[rgba(255,255,255,0.03)]">
-                  <Icon name="plus-icon" className="text-kpl-el-3" />
-                </span>
-                <span className="text-kpl-el-1 group-hover:text-kpl-el-3 text-sm">Connect More</span>
-              </Link>
+            <div>
+              <div className="flex px-3 py-2 flex-col gap-2 overflow-y-scroll">
+                <SystemEventApiTrigger
+                  isActive={currentIntegrationName === 'system'}
+                  updateCurrentIntegration={updateCurrentIntegrationItem}
+                />
+                {connectedIntegrations.map(integration => {
+                  return (
+                    <ConnectedIntegration
+                      isActive={currentIntegrationName === lowerCaseWord(integration.name)}
+                      updateCurrentIntegration={updateCurrentIntegrationItem}
+                      key={integration.name}
+                      name={integration.name}
+                      connections={integration.connections}
+                      icon={integration.icon}
+                    />
+                  );
+                })}
+                <Link
+                  href="/integrations"
+                  className="flex px-2 gap-3 py-2 group hover:bg-kpl-bg-3 transition-all items-center w-full"
+                >
+                  <span className="w-[2.25rem] grid place-items-center h-[2.25rem] rounded-xs bg-[rgba(255,255,255,0.03)]">
+                    <Icon name="plus-icon" className="text-kpl-el-3" />
+                  </span>
+                  <span className="text-kpl-el-1 group-hover:text-kpl-el-3 text-sm">Connect More</span>
+                </Link>
+              </div>
             </div>
           </TabsList>
         </div>
         <section>
           <h2 className="border-b-kpl-border-1 sticky top-0 bg-kpl-bg-3 capitalize text-sm border-b-[0.5px] py-3 p-4">
-            {currentIntegrationName} events
+            {currentIntegrationName}
           </h2>
           <TabsContent asChild value={currentIntegrationName}>
             <div className="px-4 py-3 flex flex-col gap-3 text-sm">
@@ -94,3 +102,33 @@ export const ClientLayout = ({ connectedIntegrations }: ClientLayoutProps) => {
     </Tabs>
   );
 };
+
+function SystemEventApiTrigger({
+  updateCurrentIntegration,
+  isActive,
+}: {
+  updateCurrentIntegration: (name: string) => void;
+  isActive: boolean;
+}) {
+  return (
+    <TabsTrigger asChild value={'system'}>
+      <button
+        key={'system'}
+        onClick={() => {
+          updateCurrentIntegration('system');
+        }}
+        className={cn(
+          'rounded-[0.625rem] h-14 w-full px-2 transition-all flex gap-4 items-center hover:shadow-sm',
+          isActive ? 'bg-[rgba(255,255,255,0.03)]' : '',
+        )}
+      >
+        <span className={cn('bg-kpl-bg-4 shrink-0 h-7 w-7 rounded-xs grid place-items-center')}>
+          <Icon name="system" />
+        </span>
+        <p className="flex text-left flex-col">
+          <span className="capitalize text-sm">System</span>
+        </p>
+      </button>
+    </TabsTrigger>
+  );
+}
