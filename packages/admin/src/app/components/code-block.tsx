8880fb502c8d8c582401d0463229a36ec12a7abb
Kenny <adelekekehinde06@gmail.com>
2024-09-27 12:29:39 +0100

    fix: number alignment problems

--- packages/admin/src/app/components/code-block.tsx
+++ packages/admin/src/app/components/code-block.tsx
@@ -13,14 +13,18 @@ function CodeBlockDemo({
 }) {
   return (
     <CodeBlock code={code} language={language}>
-      {filename ? <div className="absolute text-sm text-kpl-el-6/50 px-6 pl-4 py-2">{filename}</div> : null}
+      {filename ? (
+        <div className="absolute  bg-kpl-bg-2 w-full text-sm text-kpl-el-6/50 px-6 pl-4 py-2">{filename}</div>
+      ) : null}
 
       <CodeBlock.Code className={cn('bg-transparent h-full p-6 rounded-xl', filename ? 'pt-10' : '')}>
         <div className="table-row">
-          <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
-          <CodeBlock.LineContent className="flex">
-            <CodeBlock.Token className="kepler-token" />
-          </CodeBlock.LineContent>
+          <div className="flex items-center">
+            <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
+            <CodeBlock.LineContent className="flex">
+              <CodeBlock.Token className="kepler-token text-sm" />
+            </CodeBlock.LineContent>
+          </div>
         </div>
       </CodeBlock.Code>
     </CodeBlock>
