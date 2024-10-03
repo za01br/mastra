5f15782067dbf9cf3d61aa11780dc6a220cb5875
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-10-03 16:43:19 +0100

    regen notion

---
+++ packages/notion/src/client/service-comments.ts
@@ -0,0 +1,82 @@
+export const comments = {
+  "getUser": {
+    "comment": "Retrieve a user",
+    "doc": "Retrieve a user"
+  },
+  "getUsers": {
+    "comment": "List all users",
+    "doc": "List all users"
+  },
+  "postDatabaseQuery": {
+    "comment": "Query a database",
+    "doc": "Query a database"
+  },
+  "postSearch": {
+    "comment": "Search by title",
+    "doc": "Search by title"
+  },
+  "getBlockChildren": {
+    "comment": "Retrieve block children",
+    "doc": "Retrieve block children"
+  },
+  "patchBlockChildren": {
+    "comment": "Append block children",
+    "doc": "Append block children"
+  },
+  "retrieveABlock": {
+    "comment": "Retrieve a block",
+    "doc": "Retrieve a block"
+  },
+  "updateABlock": {
+    "comment": "Update a block",
+    "doc": "Update a block"
+  },
+  "deleteABlock": {
+    "comment": "Delete a block",
+    "doc": "Delete a block"
+  },
+  "retrieveAPage": {
+    "comment": "Retrieve a page",
+    "doc": "Retrieve a page"
+  },
+  "patchPage": {
+    "comment": "Update page properties",
+    "doc": "Update page properties"
+  },
+  "postPage": {
+    "comment": "Create a page",
+    "doc": "Create a page"
+  },
+  "createADatabase": {
+    "comment": "Create a database",
+    "doc": "Create a database"
+  },
+  "updateADatabase": {
+    "comment": "Update a database",
+    "doc": "Update a database"
+  },
+  "retrieveADatabase": {
+    "comment": "Retrieve a database",
+    "doc": "Retrieve a database"
+  },
+  "retrieveAPageProperty": {
+    "comment": "Retrieve a page property item",
+    "doc": "Retrieve a page property item"
+  },
+  "getSelf": {
+    "comment": "Retrieve your token's bot user",
+    "doc": "Retrieve your token's bot user"
+  },
+  "retrieveAComment": {
+    "comment": "Retrieve comments",
+    "doc": "Retrieve comments\n  Retrieves a list of un-resolved [Comment objects](ref:comment-object) from a page or block."
+  },
+  "createAComment": {
+    "comment": "Create comment",
+    "doc": "Create comment\n  Creates a comment in a page or existing discussion thread."
+  },
+  "createAToken": {
+    "comment": "Create a token",
+    "doc": "Create a token\n  Creates an access token that a third-party service can use to authenticate with Notion."
+  }
+}
