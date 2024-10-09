688cb608c9bf83fc192f3c63481097c0bf6b71a0
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2024-10-09 21:42:04 +0100

    show output and output json code in tabs

--- packages/admin/src/app/(dashboard)/agents/create/page.tsx
+++ packages/admin/src/app/(dashboard)/agents/create/page.tsx
@@ -6,7 +6,7 @@ import { AgentTools } from '@/domains/agents/components/agents-integrations';
 
 export default function Page() {
   return (
-    <div className="overflow-y-scroll">
+    <div className="flex flex-col overflow-hidden">
       <div className="flex h-[var(--top-bar-height)] bg-mastra-bg-1 sticky top-0 w-full content-center items-center justify-between border-b-[0.1px] border-mastra-border-1 px-[1.31rem]">
         <div className="h-fit items-center  gap-1.5 flex">
           <span className=" bg-mastra-bg-3 rounded h-7 w-7 grid place-items-center">
@@ -15,7 +15,7 @@ export default function Page() {
           <h1 className="text-base">Create New Agent</h1>
         </div>
       </div>
-      <section className="grid h-full gap-x-[0.62rem] grid-cols-[30rem_30rem]">
+      <section className="grid flex-1 overflow-hidden gap-x-[0.62rem] grid-cols-[30rem_30rem]">
         <AgentsCreationHeader />
 
         <div className=" px-[1.31rem] py-4 ">
