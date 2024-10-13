56a112ae6da30eb510faaa408141f1d3ac2c8c60
Kenny <adelekekehinde06@gmail.com>
2024-10-13 01:43:53 +0100

    move button back

--- packages/admin/src/domains/agents/components/agents-creation-header.tsx
+++ packages/admin/src/domains/agents/components/agents-creation-header.tsx
@@ -455,7 +455,7 @@ export const AgentsCreationHeader = () => {
                       }}
                       disabled={isDisabled}
                       type="submit"
-                      className="h-7 w-full py-1 px-4 flex justify-center rounded"
+                      className="h-10 w-full py-1 px-4 flex justify-center rounded"
                     >
                       Create Agent
                     </Button>,
