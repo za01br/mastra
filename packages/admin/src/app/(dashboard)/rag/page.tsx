b6480cfff1364a3aedf5170aba7650835b467cdf
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2024-10-15 16:33:54 +0100

    setup rag list page

--- packages/admin/src/app/(dashboard)/rag/page.tsx
+++ packages/admin/src/app/(dashboard)/rag/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from '@/app/components/header';
+import RagTable from '@/domains/rag/components/rag-table';
+import { ragTableColumns } from '@/domains/rag/components/rag-table-columns';
 
 export default function RagPage() {
+  // TODO: retrieve indexes
   return (
     <div className="flex flex-col h-full overflow-hidden">
       <div className="sticky top-0">
         <Header linkText="New RAG Sync" href="/rag/create" breadcrumbLabel="RAG" />
       </div>
+      <RagTable columns={ragTableColumns} data={[]} />
     </div>
   );
 }
