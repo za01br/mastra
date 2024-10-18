8998caba681b8fba9700478c723ada16a0add591
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-10-18 17:49:03 -0400

    remove extraneous

--- packages/admin/src/lib/get-configuration.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '@/types';
-
-import config from '../../mock-data/config.json';
-
-export async function getConfig(): Promise<Config> {
-  return new Promise(res => res(config as unknown as Config));
-}
