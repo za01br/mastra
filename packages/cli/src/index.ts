8c92a9b59d13930519cb5510de9a1b6f05bc41bc
Shane Thomas <shane@codekarate.com>
2024-10-21 15:26:03 -0500

    Add postinstall welcome message

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ import { validateNextJsRoot } from './utils.js';
 const program = new Command();
 
 program
-  .version('0.1.34')
+  .version('0.1.35')
   .description('CLI for Mastra')
   .action(() => {
     validateNextJsRoot();
