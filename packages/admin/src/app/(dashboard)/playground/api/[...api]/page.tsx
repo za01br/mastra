1b40f8f3082a2267ba00af91f7bc0a28e496f8d3
Kenny <adelekekehinde06@gmail.com>
2024-10-29 12:11:10 +0100

    feat: make page header consistent

--- packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
+++ packages/admin/src/app/(dashboard)/playground/api/[...api]/page.tsx
@@ -35,7 +35,7 @@ export default function Page({
       <ApiSchemaBlock type={searchParams.name.toUpperCase()} />
       <Separator className="w-[0.5px] bg-mastra-border-1" />
       <div className="flex flex-col gap-5 rounded-[0.375rem]">
-        <div className="grow-0 relative z-20 border-[0.5px] border-mastra-border-1 rounded-[0.25rem] bg-mastra-bg-2">
+        <div className="basic-[24rem] grow-0 relative z-20 border-[0.5px] border-mastra-border-1 rounded-[0.25rem] bg-mastra-bg-2">
           <ApiCodeBlock />
           <div className="absolute left-0 text-sm rounded-bl-[0.25rem] grid place-items-center rounded-br-[0.25rem] bottom-0 w-full text-center text-mastra-el-3 bg-mastra-bg-13 h-10 py-1 px-4">
             Code block to use in your app
