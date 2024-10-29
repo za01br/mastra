0624435a8753ec38f18cabf1e7736632a74a8e9f
Kenny <adelekekehinde06@gmail.com>
2024-10-29 17:55:39 +0100

    feat: update page

--- packages/admin/src/app/(dashboard)/logs/page.tsx
+++ packages/admin/src/app/(dashboard)/logs/page.tsx
@@ -1,4 +1,7 @@
+import { LogLevel } from '@mastra/core';
 import { Metadata } from 'next';
+
+import { getLogsConfig } from '@/domains/logs/actions';
 
 import LogsClientLayout from './client-layout';
 
@@ -7,6 +10,24 @@ export const metadata: Metadata = {
   description: 'Logs ...',
 };
 
-export default function Logs() {
-  return <LogsClientLayout />;
+export default async function Logs() {
+  const defaultLogOptions = await getLogsConfig();
+  const upstashConfig = {
+    url: process.env.UPSTASH_URL ?? '',
+    apiKey: process.env.UPSTASH_API_KEY ?? '',
+  };
+  const logLevelOptions: Array<{ name: string; value: LogLevel }> = [
+    { name: 'Debug', value: LogLevel.DEBUG },
+    { name: 'Info', value: LogLevel.INFO },
+    { name: 'Error', value: LogLevel.ERROR },
+    { name: 'Warn', value: LogLevel.WARN },
+  ];
+
+  return (
+    <LogsClientLayout
+      defaultUpstashConfig={upstashConfig}
+      defaultLogOptions={defaultLogOptions}
+      logLevelOptions={logLevelOptions}
+    />
+  );
 }
