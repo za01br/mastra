5ef6fccc57bc495324772bfae10e25f7b83c7f3d
Kenny <adelekekehinde06@gmail.com>
2024-11-01 15:53:56 +0100

    feat: autofill attributes if in edit mode

--- packages/cli/admin/src/domains/rag/components/vector-provider-form.tsx
+++ packages/cli/admin/src/domains/rag/components/vector-provider-form.tsx
@@ -17,9 +17,10 @@ import Spinner from '@/components/ui/spinner';
 import { toast } from '@/lib/toast';
 import { cn } from '@/lib/utils';
 
-import { createPineconeIndex, saveVectorToConfigAction } from '../actions';
+import { createPineconeIndex, saveVectorToConfigAction, updatePineconeIndex } from '../actions';
 import { useVectorFormContext } from '../context/vector-form-context';
 import { useVectorProviderExists } from '../hooks/use-vector-provider';
+import { VectorEntity } from '../types';
 
 import { VectorProviderFormIntegration } from './vector-provider-form-integration';
 
@@ -53,20 +54,49 @@ const resyncingIntervals = [
 
 type SyncInterval = (typeof resyncingIntervals)[number]['label'];
 
-export const VectorProviderForm = () => {
-  const { apiKey, setApiKey, vectorProvider, setVectorProvider, entities } = useVectorFormContext();
+export const VectorProviderForm = ({
+  source,
+  name,
+  indexId,
+  entity,
+}: {
+  source?: 'create' | 'edit';
+  indexId?: string;
+  name?: string;
+  entity?: VectorEntity;
+}) => {
+  const { apiKey, setApiKey, vectorProvider, setVectorProvider, indexName, setIndexName, setEntities, entities } =
+    useVectorFormContext();
   const [show, setShow] = useState(true);
   const [isSaved, setIsSaved] = useState(false);
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [syncInterval, setSyncInterval] = useState<string | undefined>();
   const [cronInterval, setCronInterval] = useState(INITIAL_CRON_INTERVAL);
-
+  const [isSavedName, setIsSavedName] = useState(false);
+  const [showBanner, setShowBanner] = useState(true);
   const router = useRouter();
 
   const { exists, apiKey: envApiKey } = useVectorProviderExists({
     providerName: 'PINECONE',
   });
+
+  const isEdit = source === 'edit';
+
+  useEffect(() => {
+    if (isEdit && name) {
+      setIndexName(name);
+      setIsSavedName(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, name]);
+
+  useEffect(() => {
+    if (entity) {
+      setEntities([entity]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [entity]);
 
   const isFilled = Boolean(vectorProvider) && Boolean(apiKey);
 
@@ -113,8 +143,28 @@ export const VectorProviderForm = () => {
   const createVectorIndex = async () => {
     setLoading(true);
 
+    if (isEdit) {
+      const response = await updatePineconeIndex({
+        id: indexId,
+        vectorEntities: entities,
+        syncInterval: transformValueToCronValue(cronInterval, intervalValue),
+        name: indexName,
+      });
+
+      if (!response.ok) {
+        setLoading(false);
+        toast(response.error);
+
+        return;
+      }
+
+      toast.success('Sync successful');
+      router.push('/rag');
+      return;
+    }
+
     const response = await createPineconeIndex({
-      provider: vectorProvider || 'PINECONE',
+      name: indexName,
       vectorEntities: entities,
       syncInterval: transformValueToCronValue(cronInterval, intervalValue),
     });
@@ -139,7 +189,22 @@ export const VectorProviderForm = () => {
 
   return (
     <section className={cn('space-y-10 max-w-[36rem]')}>
-      <div className={cn('space-y-8 p-4 border border-mastra-border-1 rounded-lg', { 'opacity-80': isSaved })}>
+      {isEdit && showBanner ? (
+        <div className="rounded-lg flex justify-between items-center bg-muted p-4">
+          <p className="text-xs text-muted-foreground flex items-center gap-1">
+            <Icon name="info" className="w-3 text-orange-400 h-3" />
+            In edit mode, you can only edit the sync interval and entities
+          </p>
+          <button
+            onClick={() => setShowBanner(false)}
+            type="button"
+            className="flex items-center w-5 h-5 rounded-lg justify-center bg-mastra-bg-5"
+          >
+            <Icon name="cancel" className="w-3 h-3" />
+          </button>
+        </div>
+      ) : null}
+      <div className={cn('space-y-8 p-4 border border-mastra-border-1 rounded-lg', { 'opacity-50': isSaved })}>
         <div className="flex justify-between items-center">
           <h2 className={cn('font-medium text-base flex items-center gap-1')}>
             <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10 text-primary">1</div>
@@ -150,17 +215,18 @@ export const VectorProviderForm = () => {
               <span className="text-xs">Configure your vector store connection</span>
             </span>
           </h2>
-
-          <Button
-            className={cn('opacity-100', { ' !cursor-not-allowed opacity-50': !isFilled })}
-            variant="outline"
-            size="xs"
-            type="button"
-            disabled={apiKey === ''}
-            onClick={updateLocalProvider}
-          >
-            {isSaved ? 'Edit' : 'Save'}
-          </Button>
+          {isEdit ? null : (
+            <Button
+              className={cn('opacity-100', { ' !cursor-not-allowed opacity-50': !isFilled })}
+              variant="outline"
+              size="xs"
+              type="button"
+              disabled={apiKey === ''}
+              onClick={updateLocalProvider}
+            >
+              {isSaved ? 'Edit' : 'Save'}
+            </Button>
+          )}
         </div>
 
         <div className="space-y-1">
@@ -222,6 +288,53 @@ export const VectorProviderForm = () => {
         </div>
       </div>
 
+      <div className={cn('space-y-8 p-4 border border-mastra-border-1 rounded-lg', { 'opacity-50': isSavedName })}>
+        <div className="flex items-center justify-between">
+          <h2 className={cn('font-medium text-base flex items-center gap-1')}>
+            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10 text-primary">2</div>
+            <span className="inline-flex flex-col">
+              <span className=" text-lg inline-flex items-center gap-2">
+                Create an Index{' '}
+                {!isSaved ? (
+                  <Icon name="lock" className="w-3 h-3" />
+                ) : (
+                  <Icon name="check-in-circle" className="text-mastra-bg-7 h-3 w-3" />
+                )}
+              </span>
+              <span className="text-xs">Configure index name</span>
+            </span>
+          </h2>
+          {isEdit ? null : (
+            <Button
+              className={cn('opacity-100', { ' !cursor-not-allowed opacity-50': !isSaved })}
+              variant="outline"
+              size="xs"
+              type="button"
+              disabled={!isSaved}
+              onClick={() => setIsSavedName(prev => !prev)}
+            >
+              {isSavedName ? 'Edit' : 'Save'}
+            </Button>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <Label className="text-gray-400 flex flex-col items-start gap-1 text-xs">
+            <p>Name</p>
+            <Input
+              type="text"
+              className="placeholder:text-xs w-full py-5 bg-white/5 overflow-ellipsis"
+              placeholder={''}
+              autoComplete="false"
+              autoCorrect="false"
+              onChange={e => setIndexName(e.target.value)}
+              value={indexName}
+              disabled={!vectorProvider || isSavedName}
+            />
+          </Label>
+        </div>
+      </div>
+
       <div className={cn('space-y-8 p-4 border border-mastra-border-1 rounded-lg', { 'opacity-50': !isSaved })}>
         <div className="flex items-center justify-between">
           <h2 className={cn('font-medium text-base flex items-center gap-1')}>
@@ -230,7 +343,7 @@ export const VectorProviderForm = () => {
               <span className=" text-lg inline-flex items-center gap-2">
                 Select Entities {!isSaved ? <Icon name="lock" className="w-3 h-3" /> : null}
               </span>
-              <span className="text-xs">Configure sync interval and entities</span>
+              <span className="text-xs">Configure sync interval and select entities to put in the index</span>
             </span>
           </h2>
         </div>
@@ -317,7 +430,7 @@ export const VectorProviderForm = () => {
         ) : (
           <span className="flex items-center gap-3">
             <Icon name="play-circle" />
-            Start sync
+            {isEdit ? 'Resync' : 'Create sync'}
           </span>
         )}
       </Button>
