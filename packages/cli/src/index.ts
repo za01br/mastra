e2ac3a2f38f3d2c9da03325d653c621658809937
Shane Thomas <shane@codekarate.com>
2024-11-07 15:53:02 -0600

    Bump CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -11,7 +11,7 @@ import { validateNextJsRoot } from './utils.js';
 const program = new Command();
 
 program
-  .version('0.1.53')
+  .version('0.1.54')
   .description('CLI for Mastra')
   .action(() => {
     validateNextJsRoot();
