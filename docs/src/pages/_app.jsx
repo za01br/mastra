9dff5c6cae87454e1eb0cfac4f2b5d9bb5959bd0
Kenny <adelekekehinde06@gmail.com>
2024-11-14 15:07:34 +0100

    feat: use geist sans

--- docs/src/pages/_app.jsx
+++ docs/src/pages/_app.jsx
@@ -1,5 +1,20 @@
+import localFont from 'next/font/local';
+
 import '../global.css';
 
+const geistSans = localFont({ src: './font/GeistVF.woff', variable: '--font-geist-sans' });
+
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${geistSans.style.fontFamily};
+        }
+      `}</style>
+      <main className={`${geistSans.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
