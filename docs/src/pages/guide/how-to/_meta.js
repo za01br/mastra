6adcfef667f4d39177d17a112ad1601028b3bff9
Kenny <adelekekehinde06@gmail.com>
2024-11-14 14:47:49 +0100

    feat: add meta for how to and remove redudant docs

---
+++ docs/src/pages/guide/how-to/_meta.js
@@ -0,0 +1,11 @@
+const meta = {
+  '01-creating-agents': 'Creating Agents',
+  '02-building-workflows': 'Building Workflows',
+  '03-knowledge-sources': 'Knowledge Sources',
+  '04-logging-and-telemetry': 'Logging and Telemetry',
+  '05-running-evals': 'Running Evals',
+  '06-adding-integrations': 'Adding Integrations',
+  '07-deployment': 'Deployment',
+};
+
+export default meta;
