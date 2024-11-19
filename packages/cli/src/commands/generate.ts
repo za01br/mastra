075449e77ee5fe3e562f383102e934f4e278ff58
Kenny <adelekekehinde06@gmail.com>
2024-11-19 16:58:30 +0100

    feat: update generate command

--- packages/cli/src/commands/generate.ts
+++ packages/cli/src/commands/generate.ts
@@ -1,24 +1,26 @@
-import dotenv from 'dotenv';
 import { execa } from 'execa';
-import fs from 'fs';
-import path from 'path';
 
-import { findFirstDirectory, getPrismaBinPath, getPrismaFilePath } from '../utils.js';
+import { getEnginePath } from '../utils.js';
+import getPackageManager from '../utils/getPackageManager.js';
 
 export async function generate(dbUrl: string) {
-  await generatePrismaClient(dbUrl);
-  // TODO: We might need to rethink this
-  // await generateTypes();
+  await generateDrizzleClient(dbUrl);
 }
 
-async function generatePrismaClient(dbUrl: string) {
-  console.log('Generating Prisma client...');
+async function generateDrizzleClient(dbUrl: string) {
+  console.log('Generating Drizzle client...');
 
-  const PRISMA_BIN = getPrismaBinPath();
+  const enginePath = getEnginePath();
 
-  const PRISMA_SCHEMA = getPrismaFilePath('schema.prisma');
+  const packageManager = getPackageManager();
 
-  return execa(`${PRISMA_BIN} generate --schema=${PRISMA_SCHEMA}`, {
+  let runCommand = packageManager;
+
+  if (packageManager === 'npm') {
+    runCommand = `${packageManager} run`;
+  }
+
+  return execa(`cd ${enginePath} && ${runCommand} generate-pg`, {
     env: {
       ...process.env,
       DB_URL: dbUrl,
@@ -27,58 +29,4 @@ async function generatePrismaClient(dbUrl: string) {
     all: true,
     stdio: 'inherit', // inherit will pipe directly to parent process stdout/stderr
   });
-}
-
-async function generateTypes() {
-  console.log('Generating types...');
-
-  dotenv.config();
-  const pkgJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
-
-  const kplDeps = Object.keys(pkgJson.dependencies).filter(k => {
-    return k.startsWith(`@mastra`) && !['@mastra/core', '@mastra/cli'].includes(k);
-  });
-
-  const possibleCorePath = [
-    path.join(process.cwd(), 'node_modules/@mastra/core'),
-    path.join(process.cwd(), '..', 'node_modules/@mastra/core'),
-    path.join(process.cwd(), '..', '..', 'node_modules/@mastra/core'),
-    path.join(process.cwd(), '..', '..', '..', 'node_modules/@mastra/core'),
-  ];
-
-  const corePath = findFirstDirectory(possibleCorePath);
-
-  if (!corePath) {
-    throw new Error(`Couldn't find core path`);
-  }
-
-  const importConfigMap = [];
-  for (const dep of kplDeps) {
-    const Int = await import(path.join(process.cwd(), `node_modules/${dep}/dist/index.js`));
-    const IntEntry = Object.keys(Int)[0];
-
-    const inst = new Int[IntEntry]({ config: {} });
-    const name = inst.name;
-    const importName = inst.constructor.name;
-    const importStatement = `import { ${importName} } from '../../../${inst.name.toLowerCase()}'`;
-    const typeFormatName = `${name.substring(0) + name.slice(1).toLowerCase()}Format`;
-    const typeFormat = `type ${typeFormatName} = { name: "${name}" };`;
-
-    importConfigMap.push({ importStatement, typeFormat, typeFormatName, importName, name });
-  }
-
-  const writePath = path.join(corePath, 'dist', 'generated-types', 'index.d.ts');
-
-  fs.writeFileSync(
-    writePath,
-    `
-    ${importConfigMap.map(({ importStatement }) => importStatement).join(`\n`)}
-
-    export interface IntegrationMap {
-   ${importConfigMap.map(({ name, importName }) => `"${name}": ${importName};`).join(`\n`)}
-    }
-    `,
-  );
-
-  console.log('Types generated successfully.');
 }
