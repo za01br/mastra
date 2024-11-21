2c8e119e86a0d0b1748e80848241342957a49d36
Kenny <adelekekehinde06@gmail.com>
2024-11-21 12:42:49 +0100

    refactor: add new line char to spinner text

--- packages/cli/src/commands/generate.ts
+++ packages/cli/src/commands/generate.ts
@@ -4,14 +4,14 @@ import yoctoSpinner from 'yocto-spinner';
 import { getEnginePath } from '../utils.js';
 import getPackageManager from '../utils/getPackageManager.js';
 
-const spinner = yoctoSpinner({ text: 'Generating drizzle client' });
+const spinner = yoctoSpinner({ text: 'Generating drizzle client\n' });
 export async function generate(dbUrl: string) {
   try {
     spinner.start();
     await generateDrizzleClient(dbUrl);
-    spinner.success('Drizzle client generated');
+    spinner.success('Drizzle client generate\n');
   } catch (err) {
-    spinner.error('Could not generate drizzle client');
+    spinner.error('Could not generate drizzle client\n');
     console.error(err);
   }
 }
