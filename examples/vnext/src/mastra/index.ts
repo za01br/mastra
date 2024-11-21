1031283dcd136cb3a72557fa69474722d84ff692
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-11-21 19:02:02 +0100

    Fix core getTool schema inference

--- examples/vnext/src/mastra/index.ts
+++ examples/vnext/src/mastra/index.ts
@@ -18,5 +18,13 @@ export const mastra = new Mastra<typeof integrations, typeof tools, typeof syncs
 });
 
 const testTool = mastra.getTool('testTool');
+const testTool2 = mastra.getTool('testTool2');
 
-testTool.execute({ name: 'ds', message: 'ds' });
+testTool.execute({
+  message: 'Hello',
+  name: 'World',
+});
+
+testTool2.execute({
+  balance: 100,
+});
--- examples/vnext/src/mastra/tools/index.ts
+++ examples/vnext/src/mastra/tools/index.ts
@@ -3,11 +3,26 @@ import { z } from 'zod';
 
 import { integrations } from '../integrations';
 
-const testToolSchema = z.object({ name: z.string(), message: z.string() });
-
 export const testTool = createTool({
   label: 'Test Tool',
-  schema: testToolSchema,
+  schema: z.object({ name: z.string(), message: z.string() }),
+  description: `This is a test tool`,
+  executor: async ({ data, getIntegration, agents, engine, llm }) => {
+    const integration = getIntegration<typeof integrations>('GITHUB');
+
+    if (!integration) {
+      throw new Error('Integration not found');
+    }
+
+    return data;
+  },
+});
+
+export const testTool2 = createTool({
+  label: 'Test Tool',
+  schema: z.object({
+    balance: z.number(),
+  }),
   description: `This is a test tool`,
   executor: async ({ data, getIntegration, agents, engine, llm }) => {
     const integration = getIntegration<typeof integrations>('GITHUB');
