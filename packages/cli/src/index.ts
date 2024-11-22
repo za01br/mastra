7b094e04058a70038e9715e2716fe5224473b626
Kenny <adelekekehinde06@gmail.com>
2024-11-22 16:56:14 +0100

    chore(version): bump version to alpha.4

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -13,7 +13,7 @@ import { getEnv } from './utils/getEnv.js';
 import { setupEnvFile } from './utils/setupEnvFile.js';
 
 const program = new Command();
-const version = '0.1.57-alpha.3';
+const version = '0.1.57-alpha.4';
 
 program
   .version(`${version}`)
