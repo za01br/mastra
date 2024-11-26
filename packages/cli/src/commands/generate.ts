54877c1a39827a4fd1ea3b5e7ac451145bc748c7
Kenny <adelekekehinde06@gmail.com>
2024-11-26 15:48:27 +0100

    feat: update generate command to use cli node module

--- packages/cli/src/commands/generate.ts
+++ packages/cli/src/commands/generate.ts
@@ -1,7 +1,8 @@
 import { execa } from 'execa';
+import path from 'path';
 import yoctoSpinner from 'yocto-spinner';
 
-import { getEnginePath } from '../utils.js';
+import { getCliPath } from '../utils.js';
 
 const spinner = yoctoSpinner({ text: 'Generating drizzle client\n' });
 
@@ -16,20 +17,21 @@ export async function generate(dbUrl: string) {
   }
 }
 async function generateDrizzleClient(dbUrl: string) {
-  const enginePath = getEnginePath();
+  const cliPath = getCliPath()!;
+  const schemaPath = path.join(cliPath, 'dist/postgres/db/schema.js');
+  const outputPath = path.join(cliPath, 'dist/postgres/drizzle');
 
   try {
     await execa(
-      `npx drizzle-kit generate --out=./dist/postgres/drizzle --dialect=postgresql --schema=./dist/postgres/db/schema.js`,
+      'npx',
+      ['drizzle-kit', 'generate', `--out=${outputPath}`, '--dialect=postgresql', `--schema=${schemaPath}`],
       {
         env: {
           ...process.env,
           DB_URL: dbUrl,
         },
-        cwd: enginePath,
-        shell: true,
-        all: true,
-        stdio: 'inherit', // inherit will pipe directly to parent process stdout/stderr
+        cwd: cliPath,
+        stdio: 'inherit',
       },
     );
   } catch (err: unknown) {
