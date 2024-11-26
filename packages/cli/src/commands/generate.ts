10578ba69f240e5a57be1658003de9eb3feadc86
Kenny <adelekekehinde06@gmail.com>
2024-11-26 16:49:24 +0100

    Revert "Fix migrate and generate bug"

--- packages/cli/src/commands/generate.ts
+++ packages/cli/src/commands/generate.ts
@@ -1,49 +1,37 @@
-import chalk from 'chalk';
 import { execa } from 'execa';
-import path from 'path';
 import yoctoSpinner from 'yocto-spinner';
 
-import { getCliPath } from '../utils.js';
+import { getEnginePath } from '../utils.js';
 
 const spinner = yoctoSpinner({ text: 'Generating drizzle client\n' });
 
 export async function generate(dbUrl: string) {
   try {
     spinner.start();
-    const res = await generateDrizzleClient(dbUrl);
-    if (res) {
-      spinner.success('Drizzle client generated\n');
-    } else {
-      spinner.stop(`Schema already generated,\nrun ${chalk.blue.bold('migrate')}`);
-    }
+    await generateDrizzleClient(dbUrl);
+    spinner.success('Drizzle client generated\n');
   } catch (err) {
     spinner.error('Could not generate drizzle client\n');
     console.error(err);
   }
 }
 async function generateDrizzleClient(dbUrl: string) {
-  const cliPath = getCliPath();
-
-  if (!cliPath) {
-    return false;
-  }
-  const schemaPath = path.join(cliPath, 'dist/postgres/db/schema.js');
-  const outputPath = path.join(cliPath, 'dist/postgres/drizzle');
+  const enginePath = getEnginePath();
 
   try {
     await execa(
-      'npx',
-      ['drizzle-kit', 'generate', `--out=${outputPath}`, '--dialect=postgresql', `--schema=${schemaPath}`],
+      `npx drizzle-kit generate --out=./dist/postgres/drizzle --dialect=postgresql --schema=./dist/postgres/db/schema.js`,
       {
         env: {
           ...process.env,
           DB_URL: dbUrl,
         },
-        cwd: cliPath,
-        stdio: 'inherit',
+        cwd: enginePath,
+        shell: true,
+        all: true,
+        stdio: 'inherit', // inherit will pipe directly to parent process stdout/stderr
       },
     );
-    return true;
   } catch (err: unknown) {
     throw err;
   }
