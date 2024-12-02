7228bd9bdb6f0b1bc7e31e4a4403641addb67bce
Kenny <adelekekehinde06@gmail.com>
2024-12-02 15:47:04 +0100

    wip

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,18 +1,29 @@
+import { ModelConfig } from '@mastra/core';
+import { execSync } from 'node:child_process';
 import fs from 'node:fs/promises';
 import path from 'path';
 import process from 'process';
 import yoctoSpinner from 'yocto-spinner';
 
+import fsExtra from 'fs-extra/esm';
+
+type LLMProvider = 'open-ai' | 'anthropic' | 'groq';
 const s = yoctoSpinner({ text: 'Initializing project\n' });
-export async function init({}: {
+export async function init({
+  directory,
+  addExample,
+  components,
+  llmProvider,
+}: {
   directory: string;
-  components: String[];
-  llmProvider: 'open-ai' | 'anthropic' | 'groq';
+  components: string[];
+  llmProvider: LLMProvider;
   addExample: boolean;
 }) {
-  s.start('Initializing project\n');
+  // s.start('Initializing Mastra\n');
 
   const message = await checkDependencies();
+
   if (message) {
     s.stop(message);
     return;
@@ -26,99 +37,119 @@ export async function init({}: {
   }
 
   try {
-    // await createMastraDir();
-    // await writeIndexFile();
-    // await createAgentDir();
-    // await writeTestAgent();
-    console.log('Mastra mastra');
-    s.success('Mastra initialized successfully');
+    const dirPath = await createMastraDir(directory);
+    await writeIndexFile(dirPath, addExample);
+    await Promise.all(components.map(component => createComponentsDir(dirPath, component)));
+    await writeAPIKey(llmProvider);
+    if (addExample) {
+      await writeTestAgent(dirPath, llmProvider);
+    }
+
+    // s.success('Mastra initialized successfully');
   } catch (err) {
     s.error('Could not initialize mastra');
     console.error(err);
   }
 }
 
-// async function createMastraDir() {
-//   const dirPath = path.join(process.cwd(), 'src', 'mastra');
-//   try {
-//     await fs.access(dirPath);
-//   } catch (err) {
-//     fs.mkdir(dirPath);
-//   }
-// }
+async function writeAPIKey(provider: LLMProvider) {
+  let key = 'OPENAI_API_KEY';
+  switch (provider) {
+    case 'anthropic':
+      key = 'ANTHROPIC_API_KEY';
+      return;
+    case 'groq':
+      key = 'GROQ_API_KEY';
+      return;
+    default:
+      key = 'OPENAI_API_KEY';
+  }
+  execSync(`echo ${key}= >> .env.development`);
+}
+async function createMastraDir(directory: string) {
+  let dir = directory
+    .trim()
+    .split('/')
+    .filter(item => item !== '');
 
-// async function createAgentDir() {
-//   const dirPath = path.join(process.cwd(), 'src', 'mastra', 'agents');
-//   try {
-//     await fs.access(dirPath);
-//   } catch (err) {
-//     try {
-//       await fs.mkdir(dirPath);
-//     } catch (err) {
-//       throw err;
-//     }
-//   }
-// }
+  const dirPath = path.join(process.cwd(), ...dir, 'mastra');
 
-// async function writeIndexFile() {
-//   const destPath = path.join(process.cwd(), 'src', 'mastra', 'index.ts');
-//   try {
-//     await fs.writeFile(destPath, '');
-//     await fs.writeFile(
-//       destPath,
-//       `
-// import { Mastra, createLogger } from '@mastra/core';
+  await fsExtra.ensureDir(dirPath);
+  return dirPath;
+}
 
-// import { agentOne, agentTwo } from './agents/agent';
+async function createComponentsDir(dirPath: string, component: string) {
+  const componentPath = dirPath + `/${component}`;
 
-// export const mastra = new Mastra({
-//   agents: [agentOne, agentTwo],
-//   logger: createLogger({
-//     type: 'CONSOLE',
-//     level: 'INFO',
-//   }),
-// });
-// `,
-//     );
-//   } catch (err) {
-//     throw err;
-//   }
-// }
+  await fsExtra.ensureDir(componentPath);
+}
 
-// async function writeTestAgent() {
-//   const destPath = path.join(process.cwd(), 'src', 'mastra', 'agents', 'agent.ts');
-//   try {
-//     fs.writeFile(destPath, '');
-//     fs.writeFile(
-//       destPath,
-//       `
-// import { Agent } from '@mastra/core';
+async function writeIndexFile(dirPath: string, addExample: boolean) {
+  const indexPath = dirPath + '/index.ts';
+  const destPath = path.join(indexPath);
+  try {
+    await fs.writeFile(destPath, '');
+    if (!addExample) {
+      await fs.writeFile(
+        destPath,
+        `
+import { Mastra } from '@mastra/core';
 
-// export const agentOne = new Agent({
-//   name: 'Agent One',
-//   instructions: 'You know about basketball, specifically the NBA. You are a sports analyst.',
-//   model: {
-//     provider: 'ANTHROPIC',
-//     name: 'claude-3-5-sonnet-20240620',
-//     toolChoice: 'auto',
-//   },
-// });
+export const mastra = new Mastra({})
+        `,
+      );
 
-// export const agentTwo = new Agent({
-//   name: 'Agent Two',
-//   instructions: 'Do this',
-//   model: {
-//     provider: 'GROQ',
-//     name: 'llama3-groq-70b-8192-tool-use-preview',
-//     toolChoice: 'auto',
-//   },
-// });
-//     `,
-//     );
-//   } catch (err) {
-//     throw err;
-//   }
-// }
+      return;
+    }
+    await fs.writeFile(
+      destPath,
+      `
+import { Mastra, createLogger } from '@mastra/core';
+
+import { agentOne, agentTwo } from './agents/agent';
+
+export const mastra = new Mastra({
+  agents: [agentOne, agentTwo],
+  logger: createLogger({
+    type: 'CONSOLE',
+    level: 'INFO',
+  }),
+});
+`,
+    );
+  } catch (err) {
+    throw err;
+  }
+}
+
+const modelToConfigMap: Record<LLMProvider, ModelConfig> = {
+  'open-ai': { provider: 'OPEN_AI', name: 'gpt-4o', toolChoice: 'auto' },
+  anthropic: { provider: 'ANTHROPIC', name: 'claude-3-5-sonnet-20241022', toolChoice: 'auto' },
+  groq: { provider: 'GROQ', name: 'llama3-groq-70b-8192-tool-use-preview', toolChoice: 'auto' },
+};
+
+async function writeTestAgent(dirPath: string, llmProvider: LLMProvider) {
+  const destPath = dirPath + '/agents/agent.ts';
+
+  const model = modelToConfigMap[llmProvider];
+  try {
+    fs.writeFile(destPath, '');
+    fs.writeFile(
+      destPath,
+      `
+import { Agent } from '@mastra/core';
+
+export const agentOne = new Agent({
+  name: 'Agent One',
+  instructions: 'You know about basketball, specifically the NBA. You are a sports analyst.',
+  model: ${model},
+});
+    `,
+    );
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function checkInitialization() {
   const dirPath = path.join(process.cwd(), 'mastra');
