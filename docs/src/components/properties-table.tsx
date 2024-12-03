8ed99525b27d913efbfec498c17e280d78b4d242
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2024-12-03 17:59:14 +0100

    update properties table

--- docs/src/components/properties-table.tsx
+++ docs/src/components/properties-table.tsx
@@ -34,10 +34,13 @@ export const PropertiesTable: React.FC<PropertiesTableProps> = ({
   }) => {
     if (properties && properties.length > 0) {
       return (
-        <div className="flex flex-col gap-1">
-          <div className="m-2 rounded-lg flex flex-col relative my-4 border border-zinc-200 dark:border-zinc-800">
-            {properties.map((prop, idx) => (
-              <div key={idx} className="flex flex-col">
+        <div className="flex flex-col">
+          {properties.map((prop, idx) => (
+            <div
+              key={idx}
+              className="m-2 rounded-lg flex flex-col relative my-4 border border-zinc-200 dark:border-zinc-800"
+            >
+              <div className="flex flex-col">
                 <div className="cursor-pointer font-mono text-xs absolute -top-3 right-2 bg-zinc-200 dark:bg-zinc-800 dark:text-zinc-400 px-2 py-1 rounded-md text-zinc-600 z-20">
                   {prop.type}
                 </div>
@@ -62,8 +65,8 @@ export const PropertiesTable: React.FC<PropertiesTableProps> = ({
                     </div>
                   ))}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       );
     }
@@ -71,25 +74,29 @@ export const PropertiesTable: React.FC<PropertiesTableProps> = ({
 
   return (
     <div className="flex flex-col">
-      {content.map((item, index) => (
-        <div
-          key={index}
-          id={item.name}
-          className="flex flex-col gap-1 py-3 border-b first:pt-0 first:pb-3 border-zinc-200 dark:border-zinc-800"
-        >
-          <div className="flex flex-row gap-2 group items-start">
-            <h3 className="font-mono text-sm font-medium cursor-pointer">
-              {item.name}
-              <span>{item.isOptional ? "?:" : ":"}</span>
-            </h3>
-            <div className="text-sm leading-5 text-zinc-500">{item.type}</div>
+      {content.map((item, index) => {
+        const returnedValue = renderType({ properties: item.properties });
+        console.log(`returnedValue - ${index}`, item);
+        return (
+          <div
+            key={index}
+            id={item.name}
+            className="flex flex-col gap-1 py-3 border-b first:pt-0 first:pb-3 border-zinc-200 dark:border-zinc-800"
+          >
+            <div className="flex flex-row gap-2 group items-start">
+              <h3 className="font-mono text-sm font-medium cursor-pointer">
+                {item.name}
+                <span>{item.isOptional ? "?:" : ":"}</span>
+              </h3>
+              <div className="text-sm leading-5 text-zinc-500">{item.type}</div>
+            </div>
+            <div className="text-sm leading-5 text-zinc-500">
+              {item.description}
+            </div>
+            {renderType({ properties: item.properties })}
           </div>
-          <div className="text-sm leading-5 text-zinc-500">
-            {item.description}
-          </div>
-          {renderType({ properties: item.properties })}
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
