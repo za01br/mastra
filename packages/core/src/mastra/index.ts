7689af3ccfbea2740886e5058675433ea1d0f61a
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-12-04 23:17:17 +0100

    trace registered engine and vector instances

--- packages/core/src/mastra/index.ts
+++ packages/core/src/mastra/index.ts
@@ -63,6 +63,30 @@ export class Mastra<
     }
 
     /* 
+   Engine
+   */
+    if (config.engine) {
+      if (this.telemetry) {
+        this.engine = this.telemetry.traceClass(config.engine);
+      } else {
+        this.engine = config.engine;
+      }
+    }
+
+    /* 
+    Vectors
+    */
+    if (config.vectors) {
+      let vectors: Record<string, MastraVector> = {};
+      if (this.telemetry) {
+        vectors = this.telemetry.traceClass(config.vectors);
+      } else {
+        vectors = config.vectors;
+      }
+      this.vectors = vectors;
+    }
+
+    /* 
     Integrations 
     */
     this.integrations = new Map();
@@ -176,24 +200,6 @@ export class Mastra<
       throw new Error('Engine is required to run syncs');
     }
     this.syncs = (config.syncs || {}) as TSyncs;
-
-    /* 
-    Engine
-    */
-    if (config.engine) {
-      if (this.telemetry) {
-        this.engine = this.telemetry.traceClass(config.engine);
-      } else {
-        this.engine = config.engine;
-      }
-    }
-
-    /* 
-    Vectors
-    */
-    if (config.vectors) {
-      this.vectors = config.vectors;
-    }
   }
 
   public async sync<K extends keyof TSyncs>(
