34ac830c4419a26b48413fd7dd32360c3b634ca0
Kenny <adelekekehinde06@gmail.com>
2024-12-06 02:56:23 +0100

    feat: remove unneccessary env values

--- packages/cli/src/utils/setup-env-file.ts
+++ packages/cli/src/utils/setup-env-file.ts
@@ -4,13 +4,11 @@ import fse from 'fs-extra/esm';
 
 import { FileEnvService } from '../services/service.fileEnv.js';
 
-export async function setupEnvFile({ dbUrl, adminPort }: { dbUrl: string; adminPort?: number }) {
+export async function setupEnvFile({ dbUrl }: { dbUrl: string }) {
   const envPath = path.join(process.cwd(), '.env.development');
 
   await fse.ensureFile(envPath);
 
   const fileEnvService = new FileEnvService(envPath);
   await fileEnvService.setEnvValue('DB_URL', dbUrl);
-  await fileEnvService.setEnvValue('APP_URL', 'http://localhost:3000');
-  await fileEnvService.setEnvValue('MASTRA_ADMIN_PORT', `${adminPort}`);
 }
