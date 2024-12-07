73fe2db35735590ffa8f5c07ae0a66424519f9ec
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-12-06 18:18:55 -0800

    Fix engine

--- examples/travel-app/.next/transform.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "transform.js";
-const runtime = require("./build/chunks/[turbopack]_runtime.js");
-runtime.loadChunk("build/chunks/[root of the server]__513b22._.js");
-runtime.loadChunk("build/chunks/postcss_config_mjs_transform_ts_2461bc._.js");
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/examples/travel-app/postcss.config.mjs/transform.ts { CONFIG => \\\"[project]/examples/travel-app/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
