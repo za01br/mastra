abc2df5f3039329e4a310c3429b5ce686222cccd
Sam Bhagwat <calcsam@gmail.com>
2024-12-09 21:44:45 -0800

    updates to agent docs

--- docs/src/pages/guide/creating-agents/_meta.js
+++ docs/src/pages/guide/creating-agents/_meta.js
@@ -1,6 +1,7 @@
 const meta = {
   "00-overview": "Overview",
-  "01-chef-michel": "Guide: Chef Michel",
+  "01-agent-memory": "Agent Memory",
+  "02-chef-michel": "Guide: Chef Michel",
 };
 
 export default meta;
