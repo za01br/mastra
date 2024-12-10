a12f40ad5beeb475e835db95f2f23d2c4556cab4
Sam Bhagwat <calcsam@gmail.com>
2024-12-10 00:16:36 -0800

    space

--- docs/src/pages/guide/creating-agents/_meta.js
+++ docs/src/pages/guide/creating-agents/_meta.js
@@ -3,7 +3,7 @@ const meta = {
   "01-agent-memory": "Agent Memory",
   "02-adding-tools": "Adding Tools",
   "03-chef-michel": "Guide: Chef Michel",
-  "04-stock-agent": "Guide:Stock Agent",
+  "04-stock-agent": "Guide: Stock Agent",
 };
 
 export default meta;
