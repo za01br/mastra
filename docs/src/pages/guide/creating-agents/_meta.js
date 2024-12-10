3de9cdf69e29d246046f8bc29a57d1dc73d3795f
Sam Bhagwat <calcsam@gmail.com>
2024-12-09 23:39:07 -0800

    move tools under agents

--- docs/src/pages/guide/creating-agents/_meta.js
+++ docs/src/pages/guide/creating-agents/_meta.js
@@ -1,7 +1,9 @@
 const meta = {
   "00-overview": "Overview",
   "01-agent-memory": "Agent Memory",
-  "02-chef-michel": "Guide: Chef Michel",
+  "02-adding-tools": "Adding Tools",
+  "03-chef-michel": "Guide: Chef Michel",
+  "04-stock-agent": "Guide:Stock Agent",
 };
 
 export default meta;
