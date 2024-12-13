522f98e094049278d0965a43b2c9e8b582dcd750
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-12-12 17:06:10 -0800

    moar

--- packages/cli/src/commands/init/utils.ts
+++ packages/cli/src/commands/init/utils.ts
@@ -97,7 +97,7 @@ import { Mastra, createLogger } from '@mastra/core';
 import { catOne } from './agents/index';
 
 export const mastra = new Mastra({
-  agents: [catOne],
+  agents: { catOne },
   logger: createLogger({
     type: 'CONSOLE',
     level: 'INFO',
