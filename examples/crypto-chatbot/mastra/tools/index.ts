251682ce9f310086e27a7a358d2951640528a3ca
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-12-13 20:10:42 -0800

    moar

--- examples/crypto-chatbot/mastra/tools/index.ts
+++ examples/crypto-chatbot/mastra/tools/index.ts
@@ -32,7 +32,7 @@ export const searchCryptoCoins = createTool({
 
     // If no exact match is found, return first coin that contains the keyword.
     const coin = data.filter((coin: any) =>
-      coin.name.toLowerCase().includes(keyword.toLowerCase())
+      coin.name.toLowerCase().includes(context.keyword.toLowerCase())
     );
 
     if (coin.length >= 0) {
@@ -49,7 +49,7 @@ export const getCryptoPrice = createTool({
   inputSchema: z.object({ id: z.string() }),
   description: 'Get crypto price by id',
   enableCache: true,
-  execute: async ({ data: { id } }) => {
+  execute: async ({ context: { id } }) => {
     console.log('getCryptoPrice for', id);
     const coinListUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}`;
 
@@ -77,7 +77,7 @@ export const getHistoricalCryptoPrices = createTool({
   inputSchema: z.object({ id: z.string(), days: z.number() }),
   description: 'Get historical crypto prices for use in a chart',
   enableCache: true,
-  execute: async ({ data: { id, days } }) => {
+  execute: async ({ context: { id, days } }) => {
     console.log('getHistoricalCryptoPrices for', id);
     const url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}`;
 
