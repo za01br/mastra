cb2ddae099e6e1392b28e060aa3589a80c932b3d
Shane Thomas <shane@codekarate.com>
2024-12-16 09:41:03 -0600

    Remove deprecated admin

--- packages/cli/admin/src/components/editor/props.ts
+++
@@ -1,7 +0,0 @@
-import { EditorProps } from '@tiptap/pm/view';
-
-export const TipTapEditorProps: EditorProps = {
-  attributes: {
-    class: 'h-full prose-sm prose-stone prose-headings:font-title font-default focus:outline-none max-w-full',
-  },
-};
--- packages/cli/admin/src/service/service.env.ts
+++
@@ -1,4 +0,0 @@
-export abstract class EnvService {
-  abstract getEnvValue(key: string): Promise<string | null>;
-  abstract setEnvValue(key: string, value: string): Promise<void>;
-}
