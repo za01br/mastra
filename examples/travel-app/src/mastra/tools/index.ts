0e9486363579360f994e6bc9c18ba63eea9f6ba4
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2024-12-18 14:20:23 +0100

    refactor tools

--- examples/travel-app/src/mastra/tools/index.ts
+++ examples/travel-app/src/mastra/tools/index.ts
@@ -8,15 +8,15 @@ import { Booking } from '../integrations/Booking';
 const booking = new Booking({ token: process.env.RAPID_API_KEY || '' });
 
 export const searchFlights = createTool({
-  label: 'Get Flight Info',
-  schema: z.object({
+  id: 'Get Flight Info',
+  inputSchema: z.object({
     startDate: z.string(),
     endDate: z.string(),
     origin: z.string(),
     destination: z.string(),
   }),
   description: `Fetches flight information for a given date range, origin and destination. Origin and Destination are Airport codes like DFW.AIRPORT or SEA.AIRPORT`,
-  executor: async ({ data: { startDate, endDate, origin, destination } }) => {
+  execute: async ({ context: { startDate, endDate, origin, destination } }) => {
     console.log('Using tool to fetch flight information: ', startDate, endDate, origin, destination);
     const flights = await booking.getFlights({ startDate, endDate, origin, destination });
     return {
@@ -26,14 +26,14 @@ export const searchFlights = createTool({
 });
 
 export const searchHotels = createTool({
-  label: 'Search Hotels',
-  schema: z.object({
+  id: 'Search Hotels',
+  inputSchema: z.object({
     startDate: z.string(),
     endDate: z.string(),
     destination: z.string(),
   }),
   description: `Searches for hotels in a specified location. Destination is a cityId like 20015732 for 20015733`,
-  executor: async ({ data: { startDate, endDate, destination } }) => {
+  execute: async ({ context: { startDate, endDate, destination } }) => {
     console.log('Using tool to search hotels: ', startDate, endDate, destination);
     return {
       hotels: (await booking.getHotels({ startDate, endDate, destination })) as Hotel[],
@@ -42,12 +42,12 @@ export const searchHotels = createTool({
 });
 
 export const searchAttractions = createTool({
-  label: 'Search Attractions',
-  schema: z.object({
+  id: 'Search Attractions',
+  inputSchema: z.object({
     destination: z.string(),
   }),
   description: `Searches for attractions in a specified location. Destination is a cityId like 20015732 for 20015733`,
-  executor: async ({ data: { destination } }) => {
+  execute: async ({ context: { destination } }) => {
     console.log('Using tool to search attractions: ', destination);
     const attractions = await booking.getAttractions({ destination });
     return {
