a14248c3d7e5ac321a056886785f4cbcce590de6
dayo <ifedayoadeniyi@gmail.com>
2024-12-23 19:06:06 +0100

    feat(dane): add commit confirmation and execution

--- examples/dane/src/mastra/workflows/commit-message.ts
+++ examples/dane/src/mastra/workflows/commit-message.ts
@@ -1,4 +1,6 @@
+import { confirm } from '@inquirer/prompts';
 import { Step, Workflow } from '@mastra/core';
+import chalk from 'chalk';
 import { execSync } from 'child_process';
 import { z } from 'zod';
 
@@ -18,7 +20,7 @@ const getDiff = new Step({
     const repoPath = context?.machineContext?.triggerData?.repoPath;
 
     // Get the git diff of staged changes
-    const diff = execSync('git diff', {
+    const diff = execSync('git diff --staged', {
       cwd: repoPath,
       encoding: 'utf-8',
     });
@@ -31,11 +33,12 @@ const generateMessage = new Step({
   id: 'generateMessage',
   outputSchema: z.object({
     commitMessage: z.string(),
+    generated: z.boolean(),
   }),
   execute: async ({ context, mastra }) => {
     const parentStep = context?.machineContext?.stepResults?.getDiff;
     if (!parentStep || parentStep.status !== 'success') {
-      return { commitMessage: '' };
+      return { commitMessage: '', generated: false };
     }
 
     const daneCommitGenerator = mastra?.agents?.daneCommitMessage;
@@ -44,6 +47,9 @@ const generateMessage = new Step({
       `
         Given this git diff:
         ${parentStep.payload.diff}
+
+        IF THE DIFF IS EMPTY, RETURN "No staged changes found", AND SET GENERATED TO FALSE,
+        OTHERWISE, SET GENERATED TO TRUE
 
         Please generate a concise and descriptive commit message that follows these guidelines:
         - Start with a verb in the present tense
@@ -55,25 +61,67 @@ const generateMessage = new Step({
       {
         schema: z.object({
           commitMessage: z.string(),
+          generated: z.boolean(),
         }),
       },
     );
 
-    return { commitMessage: res?.object?.commitMessage as string };
+    if (!res?.object?.generated) {
+      throw new Error(res?.object?.commitMessage as string);
+    }
+
+    return { commitMessage: res?.object?.commitMessage as string, generated: res?.object?.generated as boolean };
   },
 });
 
-const displayMessage = new Step({
-  id: 'displayMessage',
+const confirmationStep = new Step({
+  id: 'confirmation',
+  outputSchema: z.object({
+    confirm: z.boolean(),
+  }),
   execute: async ({ context }) => {
     const parentStep = context?.machineContext?.stepResults?.generateMessage;
     if (!parentStep || parentStep.status !== 'success') {
-      return;
+      return { confirm: false };
     }
 
-    console.log('Generated commit message:');
-    console.log(parentStep.payload.commitMessage);
+    if (!parentStep.payload.generated) {
+      return { confirm: false };
+    }
+
+    const commitMessage = parentStep.payload.commitMessage;
+
+    const confirmationMessage = await confirm({
+      message: `\n Would you like to use this commit message? \n\n ${chalk.yellow(commitMessage)}\n\n`,
+    });
+
+    return { confirm: confirmationMessage };
   },
 });
 
-commitMessageGenerator.step(getDiff).then(generateMessage).then(displayMessage).commit();
+const commitStep = new Step({
+  id: 'commit',
+  outputSchema: z.object({
+    commit: z.boolean(),
+  }),
+  execute: async ({ context }) => {
+    const parentStep = context?.machineContext?.stepResults?.confirmation;
+    if (!parentStep || parentStep.status !== 'success' || !parentStep.payload.confirm) {
+      throw new Error('Commit message generation cancelled');
+    }
+
+    if (context?.machineContext?.stepResults?.generateMessage?.status !== 'success') {
+      throw new Error('Failed to generate commit message');
+    }
+
+    const commitMessage = context?.machineContext?.stepResults?.generateMessage?.payload?.commitMessage;
+    execSync(`git commit -m "${commitMessage}"`, {
+      cwd: context?.machineContext?.triggerData?.repoPath,
+      encoding: 'utf-8',
+    });
+
+    return { commit: true };
+  },
+});
+
+commitMessageGenerator.step(getDiff).then(generateMessage).then(confirmationStep).then(commitStep).commit();
