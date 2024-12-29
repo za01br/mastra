6aca7410d58f403a79aec9b5ea06414d9b101e2c
Sam Bhagwat <calcsam@gmail.com>
2024-12-29 01:28:17 -0800

    modify sidebar title

--- docs/src/pages/guide/guides/_meta.js
+++ docs/src/pages/guide/guides/_meta.js
@@ -1,7 +1,7 @@
 const meta = {
   "01-harry-potter": "LLM Models: Harry Potter",
   "02-chef-michel": "Agents: Chef Michel",
-  "03-stock-agent": "Agents: Stock Agent",
+  "03-stock-agent": "Tools: Stock Agent",
   "04-scraper": "Workflows: Scraper",
 };
 
