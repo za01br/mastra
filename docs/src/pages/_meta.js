7a3fad5f85240115f4b5ecd3184ed43ccf238174
Kenny <adelekekehinde06@gmail.com>
2024-12-30 17:46:47 +0100

    fix: move files out of docs bucket

--- docs/src/pages/_meta.js
+++ docs/src/pages/_meta.js
@@ -1,12 +1,39 @@
 const meta = {
-  showcase: {
-    title: "Showcase",
-    type: "page",
+  index: "Introduction",
+  "getting-started": {
+    title: "Getting Started",
+    collapsed: false,
   },
-  guide: {
-    title: "Docs",
-    type: "page",
+  guides: {
+    title: "Guides",
+    collapsed: false,
   },
+  "llm-models": {
+    title: "LLM Models",
+    collapsed: false,
+  },
+  agents: {
+    title: "Agents",
+    collapsed: false,
+  },
+  workflows: {
+    title: "Workflows",
+    collapsed: false,
+  },
+  rag: {
+    title: "RAG",
+    collapsed: false,
+  },
+  "local-dev": {
+    title: "Local Dev",
+    collapsed: false,
+  },
+  deployment: {
+    title: "Deployment",
+    collapsed: false,
+  },
+  "08-running-evals": "Evals",
+  reference: "Reference",
 };
 
 export default meta;
