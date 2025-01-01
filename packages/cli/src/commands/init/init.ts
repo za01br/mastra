01cd63bd3fd5bd5bbe733b0e306599bef226c049
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-01-01 19:37:29 +0100

    Pull mastra create from init

--- packages/cli/src/commands/init/init.ts
+++ packages/cli/src/commands/init/init.ts
@@ -1,4 +1,5 @@
-import { logger } from '../../utils/logger.js';
+import * as p from '@clack/prompts';
+import color from 'picocolors';
 
 import {
   Components,
@@ -10,24 +11,27 @@ import {
   writeIndexFile,
 } from './utils.js';
 
+const s = p.spinner();
+
 export const init = async ({
   directory,
   addExample = false,
   components,
   llmProvider = 'openai',
-  showSpinner = false,
 }: {
   directory: string;
   components: string[];
   llmProvider: LLMProvider;
   addExample: boolean;
-  showSpinner?: boolean;
 }) => {
+  s.start('Initializing Mastra');
+
   try {
     const result = await createMastraDir(directory);
 
     if (!result.ok) {
-      logger.info('Mastra already initialized');
+      p.outro(color.inverse(' Mastra already initialized '));
+      s.stop();
       return { success: false };
     }
 
@@ -42,10 +46,12 @@ export const init = async ({
     if (addExample) {
       await Promise.all([components.map(component => writeCodeSample(dirPath, component as Components, llmProvider))]);
     }
-    showSpinner && logger.success('Mastra initialized successfully');
+    s.stop();
+    p.outro(' Mastra initialized successfully');
     return { success: true };
   } catch (err) {
-    showSpinner && logger.error('Could not initialize mastra');
+    s.stop();
+    p.outro(color.inverse(' Could not initialize mastra '));
     console.error(err);
     return { success: false };
   }
