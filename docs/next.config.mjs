425e2ae750f7bd85e1561354108af6c7ebffe855
Kenny <adelekekehinde06@gmail.com>
2025-01-02 09:40:56 +0100

    feat: remove unused config values

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,26 +1,19 @@
 /** @type {import('next').NextConfig} */
-import nextra from "nextra";
+import nextra from 'nextra';
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
   mdxOptions: {
     rehypePrettyCodeOptions: {
       theme: {
-        dark: "github-dark",
-        light: "github-light",
+        dark: 'github-dark',
+        light: 'github-light',
       },
     },
   },
 });
 
 export default withNextra({
-  basePath: "",
   trailingSlash: false,
-  rewrites: async () => [
-    {
-      source: "/docs",
-      destination: "/docs/root",
-    },
-  ],
 });
