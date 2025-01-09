cb588da7478c8fcfb953932eafff9bdfd0ed76f8
NikAiyer <nick.aiyer@gmail.com>
2025-01-08 21:50:12 -0600

    added rag examples and docs pages

--- docs/src/pages/examples/rag/_meta.ts
+++ docs/src/pages/examples/rag/_meta.ts
@@ -11,6 +11,11 @@ const meta = {
   "insert-embedding-in-pinecone": "Insert Embedding in Pinecone",
   "insert-embedding-in-pgvector": "Insert Embedding in PgVector",
   "retrieve-results": "Retrieve Results",
+  "basic-rag": "Building a RAG System with Mastra",
+  "cleanup-rag":
+    "Building an Enhanced RAG System with Mastra: Optimizing Information Density",
+  "filter-rag":
+    "Building an Enhanced RAG System with Mastra: Filtering Context",
 };
 
 export default meta;
