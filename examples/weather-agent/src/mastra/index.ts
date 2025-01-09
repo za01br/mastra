eb59f01594f434a368cedb4b3755b0c0e2c38ba7
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2025-01-09 20:41:32 +0100

    register and finalize example

--- examples/weather-agent/src/mastra/index.ts
+++ examples/weather-agent/src/mastra/index.ts
@@ -1,15 +1,29 @@
 import { Mastra } from '@mastra/core';
 
 import { weatherAgent } from './agents';
+import { weatherWorkflow } from './workflows';
 
 const mastra = new Mastra({
   agents: { weatherAgent },
+  workflows: { weatherWorkflow },
 });
 
-async function main() {
+async function agentExample() {
   const agent = await mastra.getAgent('weatherAgent');
   const result = await agent.generate('What is the weather in London?');
   console.log(result.text);
 }
 
-main();
+async function workflowExample() {
+  const workflow = mastra.getWorkflow('weatherWorkflow');
+  const result = await workflow.execute({
+    triggerData: {
+      city: 'London',
+    },
+  });
+  console.log('\n \n');
+  console.log(result);
+}
+
+// agentExample();
+workflowExample();
