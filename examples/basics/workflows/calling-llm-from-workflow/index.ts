4887c558552440cdf1560f0660dcc9d4a8fa3d96
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-09 21:37:12 -0800

    Test release

--- examples/basics/workflows/calling-llm-from-workflow/index.ts
+++ examples/basics/workflows/calling-llm-from-workflow/index.ts
@@ -1,46 +1,52 @@
 import { Mastra, Step, Workflow } from '@mastra/core';
 import { z } from 'zod';
 
-const newWorkflow = new Workflow({
-  name: 'pass message to the workflow',
-  triggerSchema: z.object({
-    message: z.string(),
-  }),
-});
+async function main() {
+  const newWorkflow = new Workflow({
+    name: 'pass message to the workflow',
+    triggerSchema: z.object({
+      message: z.string(),
+    }),
+  });
 
-const replyAsPenguin = new Step({
-  id: 'reply',
-  outputSchema: z.object({
-    reply: z.string(),
-  }),
-  execute: async ({ context, mastra }) => {
-    const penguinCharacter = mastra?.llm?.({
-      provider: 'OPEN_AI',
-      name: 'gpt-4o',
-    });
+  const replyAsPenguin = new Step({
+    id: 'reply',
+    outputSchema: z.object({
+      reply: z.string(),
+    }),
+    execute: async ({ context, mastra }) => {
+      const penguinCharacter = mastra?.llm?.({
+        provider: 'OPEN_AI',
+        name: 'gpt-4o',
+      });
 
-    const res = await penguinCharacter?.stream(context.machineContext?.triggerData?.message);
+      const res = await penguinCharacter?.stream(context.machineContext?.triggerData?.message);
 
-    if (!res) {
-      return { reply: '' };
-    }
+      if (!res) {
+        return { reply: '' };
+      }
 
-    for await (const chunk of res?.textStream) {
-      process.stdout.write(chunk);
-    }
+      for await (const chunk of res?.textStream) {
+        process.stdout.write(chunk);
+      }
 
-    const text = await res.text;
-    return { reply: text };
-  },
-});
+      const text = await res.text;
+      return { reply: text };
+    },
+  });
 
-newWorkflow.step(replyAsPenguin);
-newWorkflow.commit();
+  newWorkflow.step(replyAsPenguin);
+  newWorkflow.commit();
 
-const mastra = new Mastra({
-  workflows: { newWorkflow },
-});
+  const mastra = new Mastra({
+    workflows: { newWorkflow },
+  });
 
-await mastra
-  .getWorkflow('newWorkflow')
-  .execute({ triggerData: { message: 'Give me a speech as skipper from penguin of madagascar' } });
+  const { runId, start } = mastra.getWorkflow('newWorkflow').createRun();
+
+  const result = await start({ triggerData: { message: 'Give me a speech as skipper from penguin of madagascar' } });
+
+  console.log(result.results);
+}
+
+main();
