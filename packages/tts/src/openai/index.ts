836fb3a64f98d1e61e6d5eaa407bc41b688e7eea
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-10 14:56:16 -0800

    tts moves openai

---
+++ packages/tts/src/openai/index.ts
@@ -0,0 +1,63 @@
+import { MastraTTS } from '@mastra/core';
+import OpenAI from 'openai';
+import { PassThrough } from 'stream';
+
+interface OpenAITTSConfig {
+  name: 'tts-1' | 'tts-1-hd';
+  apiKey?: string;
+}
+
+export class OpenAITTS extends MastraTTS {
+  client: OpenAI;
+  constructor({ model }: { model: OpenAITTSConfig }) {
+    super({
+      model: {
+        provider: 'OPENAI',
+        ...model,
+      },
+    });
+
+    this.client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || this.model.apiKey });
+  }
+
+  async voices() {
+    const res = this.traced(() => ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'], 'tts.openai.voices')();
+    return res;
+  }
+
+  async generate({ voice, text, speed }: { voice: string; text: string; speed?: number }) {
+    const audio = await this.traced(async () => {
+      const response = await this.client.audio.speech.create({
+        model: this.model.name,
+        voice: voice as any,
+        input: text,
+        speed: speed || 1.0,
+      });
+
+      // Get the full buffer
+      const buffer = await response.arrayBuffer();
+      return Buffer.from(buffer);
+    }, 'tts.openai.generate')();
+
+    return {
+      audioResult: audio,
+    };
+  }
+
+  async stream({ voice, text, speed }: { voice: string; text: string; speed?: number }) {
+    const audio = await this.traced(async () => {
+      const response = await this.client.audio.speech.create({
+        model: this.model.name || 'tts-1',
+        voice: voice as any,
+        input: text,
+        speed: speed || 1.0,
+      });
+
+      return response.body as unknown as PassThrough;
+    }, 'tts.openai.stream')();
+
+    return {
+      audioResult: audio,
+    };
+  }
+}
