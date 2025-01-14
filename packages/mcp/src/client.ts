b3913e5f533c03a3b0f9d72e249aa6e525b15b16
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-13 20:25:59 -0800

    MCP

--- packages/mcp/src/client.ts
+++ packages/mcp/src/client.ts
@@ -4,79 +4,76 @@ import { StdioClientTransport, StdioServerParameters } from '@modelcontextprotoc
 import { ClientCapabilities, ListResourcesResultSchema } from '@modelcontextprotocol/sdk/types.js';
 
 export class MastraMCPClient {
-  name: string;
-  private transport: StdioClientTransport;
-  private client: Client;
-  constructor({
-    name,
-    version = '1.0.0',
-    server,
-    capabilities = {},
-  }: {
     name: string;
-    server: StdioServerParameters;
-    capabilities?: ClientCapabilities;
-    version?: string;
-  }) {
-    this.name = name;
-    this.transport = new StdioClientTransport(server);
-
-    this.client = new Client(
-      {
+    private transport: StdioClientTransport;
+    private client: Client;
+    constructor({
         name,
-        version,
-      },
-      {
-        capabilities,
-      },
-    );
-  }
+        version = '1.0.0',
+        server,
+        capabilities = {},
+    }: {
+        name: string;
+        server: StdioServerParameters;
+        capabilities?: ClientCapabilities;
+        version?: string;
+    }) {
+        this.name = name;
+        this.transport = new StdioClientTransport(server);
 
-  async connect() {
-    return await this.client.connect(this.transport);
-  }
+        this.client = new Client(
+            {
+                name,
+                version,
+            },
+            {
+                capabilities,
+            },
+        );
+    }
 
-  async disconnect() {
-    return await this.client.close();
-  }
+    async connect() {
+        return await this.client.connect(this.transport);
+    }
 
-  async resources() {
-    return await this.client.request({ method: 'resources/list' }, ListResourcesResultSchema);
-  }
+    async disconnect() {
+        return await this.client.close();
+    }
 
-  async tools() {
-    const { tools } = await this.client.listTools();
-    const toolsRes: Record<string, any> = {};
-    tools.forEach(tool => {
-      const s = jsonSchemaToModel(tool.inputSchema);
-      const mastraTool = createTool({
-        id: `${this.name}_${tool.name}`,
-        description: tool.description,
-        inputSchema: s,
-        execute: async ({ context }) => {
-          try {
-            const res = await this.client.callTool({
-              name: tool.name,
-              arguments: {
-                ...context,
-                model: 5000,
-              },
+    async resources() {
+        return await this.client.request({ method: 'resources/list' }, ListResourcesResultSchema);
+    }
+
+    async tools() {
+        const { tools } = await this.client.listTools();
+        const toolsRes: Record<string, any> = {};
+        tools.forEach(tool => {
+            const s = jsonSchemaToModel(tool.inputSchema);
+            const mastraTool = createTool({
+                id: `${this.name}_${tool.name}`,
+                description: tool.description,
+                inputSchema: s,
+                execute: async ({ context }) => {
+                    try {
+                        const res = await this.client.callTool({
+                            name: tool.name,
+                            arguments: context
+                        });
+
+                        return res;
+                    } catch (e) {
+                        console.log('Error calling tool', tool.name);
+                        console.error(e);
+                        throw e;
+                    }
+                },
             });
 
-            return res;
-          } catch (e) {
-            console.log('Error calling tool', tool.name);
-            console.error(e);
-            throw e;
-          }
-        },
-      });
+            if (tool.name) {
+                toolsRes[tool.name] = mastraTool;
+            }
+        });
 
-      if (tool.name) {
-        toolsRes[tool.name] = mastraTool;
-      }
-    });
-
-    return toolsRes;
-  }
+        return toolsRes;
+    }
 }
