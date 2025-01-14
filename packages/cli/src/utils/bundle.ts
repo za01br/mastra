706556f3cf7ea389d9d377e27e74edd2e8a6572f
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-14 12:08:16 -0800

    Fix banner cli

--- packages/cli/src/utils/bundle.ts
+++ packages/cli/src/utils/bundle.ts
@@ -217,10 +217,12 @@ export async function bundle(
       esbuildConfig.banner = {
         js: `
           import { createRequire } from "module";
-          import { fileURLToPath } from 'url';
+          import { fileURLToPath, pathToFileURL } from 'url';
           import path from 'path';
-          const require = createRequire(String(pathToFileURL(__filename)));
+
+          const __filename = fileURLToPath(import.meta.url);
           const __dirname = path.dirname(__filename);
+          const require = createRequire(import.meta.url);
         `,
       };
     }
