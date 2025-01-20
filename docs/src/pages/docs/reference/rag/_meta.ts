a0ea010997fb68e73391694d9888eaebde92dade
Kenny <adelekekehinde06@gmail.com>
2025-01-20 19:01:57 +0100

    fix: broken docs (#1436)
    Fix broken docs

--- docs/src/pages/docs/reference/rag/_meta.ts
+++ docs/src/pages/docs/reference/rag/_meta.ts
@@ -1,14 +1,12 @@
-export default {
+const meta = {
   pgstore: "PGStore",
   chunk: "Chunk",
-  "document-chunker": "createDocumentChunker()",
   document: "MDocument",
   embeddings: "embed()",
-  "graph-rag-tool": "createGraphRAGTool()",
   "graph-rag": "GraphRAG",
   pinecone: "Pinecone",
   qdrant: "Qdrant",
   reranker: "Reranker",
-  "vector-query-search": "vectorQuerySearch()",
-  "vector-query-tool": "vectorQueryTool()",
 };
+
+export default meta;
