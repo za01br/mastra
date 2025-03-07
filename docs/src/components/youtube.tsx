32465c60e05f1e48a968943b2e87b2ec3e63130b
Shane Thomas <shane@codekarate.com>
2025-03-07 00:34:21 -0800

    Don't show related youtube videos in docs (#2826)

--- docs/src/components/youtube.tsx
+++ docs/src/components/youtube.tsx
@@ -3,7 +3,7 @@ export default function YouTube({ id }: { id: string }) {
     <div className="my-4">
       <iframe
         className="aspect-video w-full"
-        src={"https://www.youtube.com/embed/" + id}
+        src={"https://www.youtube.com/embed/" + id + "?rel=0"}
         title="YouTube Video Player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
